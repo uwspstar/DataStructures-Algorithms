9d350f102ae93ccbcfaa340085546662287bb49d
xing wang <uwspstar@gmail.com>
2019-09-09 11:01:53 -0500

    Update first-repeat-item.js

--- js/first-repeat-item.js
+++ js/first-repeat-item.js
@@ -2,6 +2,7 @@
 // [2,1,3,2,5,4,1,3] return 2
 // from google
 
+// solution 1
 const firstRepeat = function (arr) {
   let map = {};
   for(let i = 0; i < arr.length; i++) {
@@ -21,6 +22,23 @@ const firstRepeat = function (arr) {
 console.dir(firstRepeat([2,1,3,2,5,4,1,3]));
 console.dir(firstRepeat([2,1,3,5,4]));
 
-
+// solution 2
+//[2,1,3,2,5,4,1,3] return 2
+const firstRepeat = function (arr) {
+  let map = {};
+  for(let i = 0; i < arr.length; i++) {
+    if(map[arr[i]]!= undefined) { 
+      // first map.2 not exist, 
+      // so map.2 = undefined
+      // if(0) is false
+      return arr[i];
+    } else {
+      map[arr[i]] = i;
+    }
+  }
+  return undefined;
+}
+console.dir(firstRepeat([2,1,3,5,4]));
+console.dir(firstRepeat([2,1,3,2,5,4,1,3]));
 
 
