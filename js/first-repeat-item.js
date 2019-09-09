3ecd7981f87f6081748bae339e0f4cdc412bb0d0
xing wang <uwspstar@gmail.com>
2019-09-09 09:44:47 -0500

    Create first-repeat-item.js

---
+++ js/first-repeat-item.js
@@ -0,0 +1,21 @@
+
+// [2,1,3,2,5,4,1,3] return 2
+// from google
+
+const firstRepeat = function (arr) {
+  let map = {};
+  for(let i = 0; i < arr.length; i++) {
+    if(!map[arr[i]]) {
+      map[arr[i]] = 1;
+    } else {
+      map[arr[i]] += 1;
+
+      if (map[arr[i]] > 1) {
+        return arr[i]
+      }
+    }
+  }
+  return null;
+}
+
+console.dir(firstRepeat([2,1,3,2,5,4,1,3]));
