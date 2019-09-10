0834db078eb90a4cb8b94dbb6e1338603817feef
xing wang <uwspstar@gmail.com>
2019-09-10 14:25:30 -0500

    Create maxSubstring.js

---
+++ js/maxSubstring.js
@@ -0,0 +1,17 @@
+const maxSubstring = function (arr) {
+
+  let maxSum = arr[0];
+  let maxCurrent = arr[0];
+  //[1,2,-3,4,-5,6]
+  for (let i = 1; i < arr.length; i++) {
+    maxCurrent = Math.max(maxCurrent + arr[i], arr[i]);
+    maxSum = Math.max(maxCurrent, maxSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubstring([1,2,3]));//6
+console.log(maxSubstring([1,2,-3,4]));//
+console.log(maxSubstring([-1,2,-3]));//2
+console.log(maxSubstring([1,2,-3,4,-5,6]));//6
+console.log(maxSubstring([1,2,3,-4,5,6])); //13
