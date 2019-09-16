c1030a39090b4e4e8acd5821b510c6530cf292f4
xing wang <uwspstar@gmail.com>
2019-09-16 03:48:26 -0500

    Update reverse-string.js

--- js/reverse-string.js
+++ js/reverse-string.js
@@ -12,3 +12,10 @@ console.dir(reverse("abc"));
 
 // console.log vs console.dir
 // log only prints out a toString representation, whereas dir prints out a navigable tree.
+
+const reverse = function (str) {
+  if (str.length === 1 ) return str; 
+  return str.substring(1) + str[0];
+}
+
+console.dir(reverse("abc"));
