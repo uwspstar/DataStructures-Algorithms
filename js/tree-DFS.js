536d21d6071384a143a90ca674ed332de7858aa5
xing wang <uwspstar@gmail.com>
2019-09-15 20:50:10 -0500

    Create tree-DFS.js

---
+++ js/tree-DFS.js
@@ -0,0 +1,15 @@
+/*
+DFS
+
+Create a variable to store the values of nodes visited
+Store the root of the BST in a variable called current
+Write a helper function which accepts a node
+If the node has a left property, call the helper function with the left property on the node
+Push the value of the node to the variable that stores the values
+If the node has a right property, call the helper function with the right property on the node
+Invoke the helper function with the current variable
+Return the array of values
+
+
+
+*/
