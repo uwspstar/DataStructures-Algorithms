f31fdef735546efef91c9a67c25d8af2508ac998
xing wang <uwspstar@gmail.com>
2019-09-16 03:24:53 -0500

    Update tree-DFS.js

--- js/tree-DFS.js
+++ js/tree-DFS.js
@@ -11,5 +11,14 @@ Invoke the helper function with the current variable
 Return the array of values
 
 
+DFS - PreOrder
 
+Create a variable to store the values of nodes visited
+Store the root of the BST in a variable called current
+Write a helper function which accepts a node
+Push the value of the node to the variable that stores the values
+If the node has a left property, call the helper function with the left property on the node
+If the node has a right property, call the helper function with the right property on the node
+Invoke the helper function with the current variable
+Return the array of values
 */
