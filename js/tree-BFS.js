7c5722be5a47e351c11f1e188ff506cd9273517b
xing wang <uwspstar@gmail.com>
2019-09-15 20:49:24 -0500

    Create tree-BFS.js

---
+++ js/tree-BFS.js
@@ -0,0 +1,20 @@
+// TRAVERSING A TREE
+// Two ways:
+
+// Breadth-first Search (BFS)
+// Depth-first Search (DFS)
+
+/*
+BFS
+
+Create a queue (this can be an array) and a variable to store the values of nodes visited
+Place the root node in the queue
+Loop as long as there is anything in the queue
+Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+If there is a left property on the node dequeued - add it to the queue
+If there is a right property on the node dequeued - add it to the queue
+Return the variable that stores the values
+
+
+
+*/
