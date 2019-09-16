26479c205469783907fe3e89198dea620337c4e1
xing wang <uwspstar@gmail.com>
2019-09-15 20:24:57 -0500

    Update sort-binary-arr.js

--- js/sort-binary-arr.js
+++ js/sort-binary-arr.js
@@ -1,10 +1,11 @@
 let arr = [1, 0, 1, 0, 1, 1, 0, 1, 0, 0];
 //i++ vs ++i
+
 function sort(arr) {
   let k = 0;
   for (let i = 0; i < arr.length; i++) {
     if(arr[i] === 0) {
-      arr[k++] = 0;
+      arr[k++] = 0; // https://www.youtube.com/watch?v=lrtcfgbUXm4
     }
     console.log('i=',i,' k=',k,arr);
   }
