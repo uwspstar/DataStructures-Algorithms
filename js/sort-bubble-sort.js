5cbf37658a41712d2596386b74357f6b55b60ad6
xing wang <uwspstar@gmail.com>
2019-09-16 19:45:09 -0500

    Update sort-bubble-sort.js

--- js/sort-bubble-sort.js
+++ js/sort-bubble-sort.js
@@ -8,3 +8,57 @@ If arr[j] is greater than arr[j+1], swap those two values!
 Return the sorted array
 
 */
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr){
+  for(var i = arr.length; i > 0; i--){
+    for(var j = 0; j < i - 1; j++){
+      console.log(arr, arr[j], arr[j+1]);
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;         
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
+
+
+// Optimized BubbleSort with noSwaps
+function bubbleSort(arr){
+  var noSwaps;
+  for(var i = arr.length; i > 0; i--){
+    noSwaps = true;
+    for(var j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;         
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
