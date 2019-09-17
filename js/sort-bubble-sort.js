a37698cca6f2471a4a73832f49670debe73e402b
xing wang <uwspstar@gmail.com>
2019-09-16 19:35:20 -0500

    Create sort-bubble-sort.js

---
+++ js/sort-bubble-sort.js
@@ -0,0 +1,10 @@
+/*
+BubbleSort Pseudocode
+ 
+
+Start looping from with a variable called i the end of the array towards the beginning
+Start an inner loop with a variable called j from the beginning until i - 1
+If arr[j] is greater than arr[j+1], swap those two values!
+Return the sorted array
+
+*/
