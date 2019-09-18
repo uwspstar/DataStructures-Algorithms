b235a557d19d9c5ab3473cc5119d3575257352fa
xing wang <uwspstar@gmail.com>
2019-09-18 09:30:05 -0500

    Update sort-insertion-sort.js

--- js/sort-insertion-sort.js
+++ js/sort-insertion-sort.js
@@ -11,9 +11,9 @@ Repeat until the array is sorted
 */
 
 
-function insertionSort(arr){
+function insertionSort(arr) {
 	var currentVal;
-    for(var i = 1; i < arr.length; i++){
+    for(var i = 1; i < arr.length; i++) {
         currentVal = arr[i];
         for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
             arr[j+1] = arr[j]
