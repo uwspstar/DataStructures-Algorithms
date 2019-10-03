2203b4a8f2967f1efbf8f27ae574c6026365f492
xing wang <uwspstar@gmail.com>
2019-10-03 10:05:14 -0500

    Update twoSum.js

--- js/twoSum.js
+++ js/twoSum.js
@@ -61,3 +61,19 @@ const twoSum = function(nums, target) {
 
 console.log(twoSum([2, 7, 11, 15],13));
 
+//Solution-1-Brute Force — O(n²)
+const twoSum = (arr, target) => {
+	var result = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = i + 1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === target) {
+				result.push(i);
+				result.push(j);
+			}
+		}
+	}
+	return result;
+}
+console.log(twoSum([2, 7, 11, 15], 9));
+// => Output [ 0, 1 ]
