5363eb5b3dcf4fb974a80bad6a8629a84898ec16
xing wang <uwspstar@gmail.com>
2019-10-03 21:33:52 -0500

    Update sort-selection-sort.js

--- js/sort-selection-sort.js
+++ js/sort-selection-sort.js
@@ -9,3 +9,19 @@ If the "minimum" is not the value (index) you initially began with, swap the two
 Repeat this with the next element until the array is sorted.
 
 */
+
+
+const selectionSort = function (arr) {
+  if(arr.length < 2) return arr;
+  for (let i = 0; i < arr.length; i++) {
+    let smallIdx = i;
+    for (let j = smallIdx; j < arr.length; j++) {
+      if (arr[smallIdx] > arr[j]) {
+        smallIdx = j;
+      }
+    }
+    [arr[i], arr[smallIdx]] = [arr[smallIdx], arr[i]];
+  }
+  return arr;
+}
+console.log(selectionSort([5,4,3,2,1]));
