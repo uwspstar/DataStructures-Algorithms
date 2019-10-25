6b67f422f818e70486b806fea2e5d3c98e708d95
xing wang <uwspstar@gmail.com>
2019-10-25 06:20:51 -0500

    Update tree-BST-insert.js

--- js/tree-BST-insert.js
+++ js/tree-BST-insert.js
@@ -1,3 +1,21 @@
+/*
+INSERTING A NODE
+Steps - Iteratively or Recursively
+
+Create a new node
+Starting at the root
+    Check if there is a root, if not - the root now becomes that new node!
+    If there is a root, check if the value of the new node is greater than or less than the value of the root
+    If it is greater 
+        Check to see if there is a node to the right
+            If there is, move to that node and repeat these steps
+            If there is not, add that node as the right property
+    If it is less
+        Check to see if there is a node to the left
+            If there is, move to that node and repeat these steps
+            If there is not, add that node as the left property
+*/
+
 class Node {
     constructor(value){
         this.value = value;
