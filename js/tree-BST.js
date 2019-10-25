c86f76581c820b1bb3d6e951293d041322072a64
xing wang <uwspstar@gmail.com>
2019-10-25 06:10:46 -0500

    Update tree-BST.js

--- js/tree-BST.js
+++ js/tree-BST.js
@@ -13,6 +13,12 @@ class Node {
     }
 }
 
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+
 /*
 INSERTING A NODE
 Steps - Iteratively or Recursively
