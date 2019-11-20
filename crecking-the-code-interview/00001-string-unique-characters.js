105f2607545455e5d113b7f111c267c6dba06959
xing wang <uwspstar@gmail.com>
2019-11-19 19:34:07 -0600

    Update 00001-string-unique-characters.js

--- crecking-the-code-interview/00001-string-unique-characters.js
+++ crecking-the-code-interview/00001-string-unique-characters.js
@@ -3,3 +3,20 @@ Is Unique:
 Implement an algorithm to determine if a string has all unique characters. 
 What if you cannot use additional data structures?
 */
+
+const isUniqueChars = function (str) {
+  if (str.length > 128) return false;
+  let map = {};
+  for(let i = 0; i < str.length; i++){
+    if(map[str[i]]) {
+      return false;
+    } else {
+      map[str[i]] = true;
+    }
+  }
+  return true;
+}
+
+console.log(isUniqueChars("aba"));
+console.log(isUniqueChars("abc"));
+console.log(isUniqueChars("0120"));
