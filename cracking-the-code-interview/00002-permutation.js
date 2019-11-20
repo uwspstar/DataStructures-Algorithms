a9a8fbb571d38591f95104f0588ba26e8672a775
xing wang <uwspstar@gmail.com>
2019-11-19 20:34:54 -0600

    Create 00002-permutation.js

---
+++ cracking-the-code-interview/00002-permutation.js
@@ -0,0 +1,13 @@
+/*
+Check Permutation: 
+Given two strings, 
+write a method to decide if one is a permutation of the other.
+*/
+const permutation = function (str1, str2) { 
+  if (str1.length !== str2.length) return false;  
+  return (str1.split('').sort().join('') !== str2.split('').sort().join(''))
+}
+
+console.log(permutation("aba","bab")); // true
+console.log(permutation("dog%","%god")); // false
+console.log(permutation("dog1"," god")); // true
