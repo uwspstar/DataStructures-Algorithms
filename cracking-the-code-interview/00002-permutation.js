fba5580cc94aaf326670382be3de9ef3b708ee14
xing wang <uwspstar@gmail.com>
2019-11-20 20:07:13 -0600

    Update 00002-permutation.js

--- cracking-the-code-interview/00002-permutation.js
+++ cracking-the-code-interview/00002-permutation.js
@@ -8,6 +8,38 @@ const permutation = function (str1, str2) {
   return (str1.split('').sort().join('') !== str2.split('').sort().join(''))
 }
 
-console.log(permutation("aba","bab")); // true
-console.log(permutation("dog%","%god")); // false
-console.log(permutation("dog1"," god")); // true
+console.log(permutation("aba","bab")); // false
+console.log(permutation("dog%","%god")); // true
+console.log(permutation("dog1"," god")); // false
+
+
+/*
+Check Permutation: 
+Given two strings, 
+write a method to decide if one is a permutation of the other.
+Check if the two strings have identical character counts.
+*/
+const permutation = function (str1, str2) { 
+  if (str1.length !== str2.length) return false; 
+  let map = {} ;
+  for (let i = 0; i < str1.length; i++) {
+    if (map[str1[i]]!== undefined) {
+      map[str1[i]]++;
+    } else {
+      map[str1[i]] = 1;
+    }
+  }
+  
+  for (let j = 0; j < str2.length; j++) {
+    if (map[str2[j]]) {
+      map[str2[j]]--; 
+    } else {
+      return false;
+    } 
+  }
+  return true;
+}
+
+console.log(permutation("aba","bab")); // false
+console.log(permutation("dog%","%god")); // true
+console.log(permutation("dog1"," god")); // false
