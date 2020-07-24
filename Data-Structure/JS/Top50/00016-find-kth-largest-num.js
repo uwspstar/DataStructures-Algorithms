322687f99167e3c819134a06ddb881b122e8499f
xing wang <uwspstar@gmail.com>
2020-07-24 13:17:20 -0500

    daily

--- Data-Structure/JS/Top50/00016-find-kth-largest-num.js
+++ Data-Structure/JS/Top50/00016-find-kth-largest-num.js
@@ -8,8 +8,10 @@ Input: [3,2,3,1,2,4,5,5,6] and k = 4 Output: 4
 //slow with bubble sort concept O(N^2)
 const findKthLargestNum = function (arr, k) {
     if (arr.length < k) return undefined;
+
     const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]] };
-    for (let i = arr.length - 1; i < k; i--) {
+
+    for (let i = arr.length - 1; i >= arr.length - k; i--) {
         let isSwap = false;
         for (let j = 0; j < i; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -17,7 +19,35 @@ const findKthLargestNum = function (arr, k) {
                 isSwap = true;
             }
         }
-        if (isSwap) break;
+        if (!isSwap) break; // not if (isSwap) break; 
     }
-    return arr[arr.length - k];
+
+    return arr.slice(-k)[0];
 }
+
+// O (NlogN)
+const findKthLargestNum = function (arr, k) {
+    if (arr.length < k) return undefined;
+
+    const merge = function (left, right) {
+        if (left.length === 0) return right;
+        if (right.length === 0) return left;
+        let result = [];
+        while (left.length > 0 && right.length > 0) {
+            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift())
+        }
+        return result.concat(left, right);
+    }
+    const mergeSort = function (arr) {
+        if (arr.length < 2) return arr; // base case, always need
+        let mid = (arr.length) >> 1;
+        let left = mergeSort(arr.slice(0, mid));
+        let right = mergeSort(arr.slice(mid));
+        return merge(left, right);
+    }
+
+    return mergeSort(arr).slice(-k)[0];
+}
+console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 4)); // 3
+console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 2)); // 5
+console.log(findKthLargestNum([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // 4
--- Data-Structure/JS/Top50/test.js
+++ Data-Structure/JS/Top50/test.js
@@ -13,24 +13,29 @@ var findKthLargest = function (arr, k) {
 //console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
 //console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
 
-
+// O (NlogN)
 const findKthLargestNum = function (arr, k) {
     if (arr.length < k) return undefined;
 
-    const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]] };
-
-    for (let i = arr.length - 1; i >= arr.length - k - 1; i--) {
-        let isSwap = false;
-        for (let j = 0; j <= i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-                //isSwap = true;
-            }
+    const merge = function (left, right) {
+        if (left.length === 0) return right;
+        if (right.length === 0) return left;
+        let result = [];
+        while (left.length > 0 && right.length > 0) {
+            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift())
         }
-        if (isSwap) break;
+        return result.concat(left, right);
     }
-    console.log('arr=', arr);
-    return arr.slice(-k)[0];
+    const mergeSort = function (arr) {
+        if (arr.length < 2) return arr; // base case, always need
+        let mid = (arr.length) >> 1;
+        let left = mergeSort(arr.slice(0, mid));
+        let right = mergeSort(arr.slice(mid));
+        return merge(left, right);
+    }
+
+    return mergeSort(arr).slice(-k)[0];
 }
-console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 2)); // 6
+console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 4)); // 3
+console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 2)); // 5
 console.log(findKthLargestNum([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // 4
