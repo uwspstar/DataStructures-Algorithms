e8f7806cde1d3a6162d552a2b5fa048c0c5b5cb4
xing wang <uwspstar@gmail.com>
2020-07-24 08:02:48 -0500

    daily

--- Data-Structure/JS/Top50/test.js
+++ Data-Structure/JS/Top50/test.js
@@ -13,3 +13,5 @@ const findKthLargestNum = function (arr, k) {
     }
     return arr[arr.length - k];
 }
+console.log(findKthLargestNum([3, 2, 1, 5, 6, 4], 2));
+console.log(findKthLargestNum([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
