83ffad9f74995efec3b241d8e4ebad33c65ee75e
xing wang <uwspstar@gmail.com>
2020-07-26 14:48:35 -0500

    daily

--- Data-Structure/Python/searching/binary-search.py
+++ Data-Structure/Python/searching/binary-search.py
@@ -2,10 +2,12 @@
 def binary_search(nums, num):
     if num < nums[0] or num > nums[-1]:
         return False
+
     start = 0
     end = len(nums) - 1
+
     while start < end:
-        mid = (nums[start] + nums[end]) >> 1
+        mid = (start + end) >> 1
         if nums[mid] == num:
             return True
         if nums[mid] > num:
@@ -16,7 +18,7 @@ def binary_search(nums, num):
     return False
 
 
-print(binary_search([1, 2, 3, 4, 5], 6)) # False
-print(binary_search([2, 3, 4, 5], 1)) # True
-print(binary_search([1, 2, 3, 4, 5], 3)) # False
-print(binary_search([1, 2, 4, 5], 3)) # False
+print(binary_search([1, 2, 3, 4, 5], 6))  # False
+print(binary_search([2, 3, 4, 5], 1))  # False
+print(binary_search([1, 2, 3, 4, 5], 3))  # True
+print(binary_search([1, 2, 4, 5], 3))  # False
