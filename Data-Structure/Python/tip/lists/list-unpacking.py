6ead9f173c78302c5b76d4d57e6a0b9e0cddb2af
xing wang <uwspstar@gmail.com>
2020-07-26 10:54:59 -0500

    daily

--- Data-Structure/Python/tip/lists/list-unpacking.py
+++ Data-Structure/Python/tip/lists/list-unpacking.py
@@ -1,8 +1,12 @@
 nums = list(range(4))
 first, second, third, fourth = nums
-print(f'first={first}')
-print(f'second={first}')
+print(f'first={first},second={second},fourth={fourth}')
 
 nums2 = list(range(4))
 first, second, *others = nums2
-print(f'others={others}')
+print(f'first={first},second={second},others={others}')
+
+
+nums3 = list(range(4))
+first, *others, last = nums3
+print(f'first={first},others={others}, last={last}')
