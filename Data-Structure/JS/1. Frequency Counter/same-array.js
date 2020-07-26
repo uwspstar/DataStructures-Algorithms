3596985f560cce416698348fe47c10019a5a51db
xing wang <uwspstar@gmail.com>
2020-07-26 07:57:13 -0500

    daily

--- Data-Structure/JS/1. Frequency Counter/same-array.js
+++ Data-Structure/JS/1. Frequency Counter/same-array.js
@@ -70,6 +70,22 @@ const same = function (arr1, arr2) {
 
     return true;
 }
+
+const same = function (arr1, arr2) {
+    if (arr1.length != arr2.length) return false;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let rightIndex = arr2.indexOf(arr1[i] ** 2);
+        if (rightIndex < 0) return false;
+        arr2.splice(rightIndex, 1);
+    }
+    return true
+}
+
+console.log(same([1, 2, 3], [4, 1, 9]));
+console.log(same([1, 2, 3], [1, 9]));
+console.log(same([1, 2, 1], [4, 4, 1]));
+
 console.log(same([1, 2, 2, 3, 3], [9, 4, 1, 4, 9])) // true
 console.log(same([1, 2, 3], [4, 1, 9])) // true
 console.log(same([1, 2, 3, 2], [4, 4, 1, 9])) // true
