b0adf2ad6e362b8e7d9d3f53e772172cf88de955
xing wang <uwspstar@gmail.com>
2020-07-26 07:48:38 -0500

    daily

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -4,100 +4,97 @@
 // mergeSort : merge(left,right), sort(arr), no swap
 // quickSort : pivot, swap
 
-const arr = [1, 2, 3, 5, 4];
-const quickSort = function (arr, low = 0, high = arr.length - 1) {
-    if (arr.length < 2) return arr;
-    const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]] };
-    const getPivotIndex = (arr, low, high) => {
-        let pivot = arr[high];
-        let pivotIndex = low;
-        for (let i = low; i < high; i++) {
-            if (arr[i] < pivot) {
-                swap(arr, i, pivotIndex);
-                pivotIndex++;
-            }
-        }
-        swap(arr, high, pivotIndex);
-        return pivotIndex;
-    }
-    if (low < high) {
-        let pi = getPivotIndex(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-    return arr;
+const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
 }
 
-console.log(quickSort(arr));
-
-/*
-const mergeSort = function (arr) {
+const quickSortHelp = (arr) => {
     if (arr.length < 2) return arr;
+
+    const getPivotIndex = (arr, low, high) => {
+        let pi = low;
+        let pivot = arr[high];
+        for (let i = low; i <= high; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, pi)
+                pi++;
+            }
+        }
+        swap(arr, high, pi)
+        return pi;
+    }
+
+    const quickSort = (arr, low = 0, high = arr.length - 1) => {
+        if (arr.length < 2) return arr;
+        if (low < high) {
+            let pi = getPivotIndex(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+        return arr;
+    }
+    return quickSort(arr);
+}
+const mergeSortHelp = (arr) => {
+    if (arr.length < 2) return arr;
+
     const merge = (left, right) => {
         if (left.length === 0) return right;
-        if (right.length === 0) return left;
+        if (left.right === 0) return left;
         let result = [];
-
         while (left.length > 0 && right.length > 0) {
-            left[0] < right[0] ?
-                result.push(left.shift()) :
-                result.push(right.shift());
+            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
         }
         return result.concat(left, right);
     }
-    let mid = arr.length >> 1;
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right);
+    const mergeSort = (arr) => {
+        if (arr.length < 2) return arr; //base class
+        let mid = arr.length >> 1;
+        let left = mergeSort(arr.slice(0, mid));
+        let right = mergeSort(arr.slice(mid));
+        return merge(left, right);
+    }
+
+    return mergeSort(arr);
 }
-const insertingSort = function (arr) {
+const insertionSort = (arr) => {
     if (arr.length < 2) return arr;
-    //const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]]};
     for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
         let j = i - 1;
+        let current = arr[i];
         for (; j >= 0; j--) {
             if (arr[j] > current) {
                 arr[j + 1] = arr[j];
-            } else break; //left side sorted
+            } else break;
         }
         arr[j + 1] = current;
     }
-
     return arr;
 }
-const selectionSort = function (arr) {
-    //get smallest each loop
+const selectionSort = (arr) => {
     if (arr.length < 2) return arr;
-    const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]] };
-
     for (let i = 0; i < arr.length; i++) {
-        let smallIdx = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[smallIdx]) {
-                smallIdx = j;
+        let smallIndex = i;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[smallIndex]) {
+                smallIndex = j;
             }
         }
-        if (smallIdx !== i) {
-            swap(arr, i, smallIdx);
-        }
+        if (smallIndex !== i) swap(arr, i, smallIndex);
     }
     return arr;
 }
-
-const bubbleSort = function (arr) {
+const bubbleSort = (arr) => {
     if (arr.length < 2) return arr;
-    const swap = (arr, i, j) => { [arr[i], arr[j]] = [arr[j], arr[i]] };
     for (let i = arr.length - 1; i >= 0; i--) {
         let isSwap = false;
         for (let j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-                isSwap = true;
-            }
+            if (arr[j] > arr[j + 1]) swap(arr, i, j);
+            isSwap = true;
         }
         if (!isSwap) break;
     }
     return arr;
 }
-*/
+
+console.log(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
