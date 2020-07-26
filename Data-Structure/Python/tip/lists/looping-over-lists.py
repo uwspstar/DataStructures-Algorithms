5586b332752e04f436f8d7833600a6ed14b377ce
xing wang <uwspstar@gmail.com>
2020-07-26 11:00:20 -0500

    daily

---
+++ Data-Structure/Python/tip/lists/looping-over-lists.py
@@ -0,0 +1,11 @@
+nums = list(range(4))
+# first, second, third, fourth = nums
+# print(f'first={first},second={second},fourth={fourth}')
+
+for index, num in enumerate(nums):
+    print(index, num)
+
+
+# tuple
+for num in enumerate(nums):
+    print(num)
