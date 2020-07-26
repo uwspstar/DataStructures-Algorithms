e9b29d6bee5b63589852532c7b656cc65768f10a
xing wang <uwspstar@gmail.com>
2020-07-26 13:58:26 -0500

    daily

---
+++ Data-Structure/Python/searching/binary-search.py
@@ -0,0 +1,3 @@
+def binary_search (arr, num) :
+    for i in range(len(arr)):
+        target = 
---
+++ Data-Structure/Python/sort/python-build-in-sort-vs-sorted.py
@@ -0,0 +1,4 @@
+# https://docs.python.org/3/howto/sorting.html#ascending-and-descending
+# 
+# Python lists have a built-in list.sort() method that modifies the list in-place. 
+# There is also a sorted() built-in function that builds a new sorted list from an iterable.
