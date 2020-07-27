3e9cef0f10983f021a2a8c5bd0489bade301cf5d
xing wang <uwspstar@gmail.com>
2020-07-27 16:33:49 -0500

    binary search

--- Data-Structure/JS/searching/Binary-Search.js
+++ Data-Structure/JS/searching/Binary-Search.js
@@ -4,187 +4,51 @@
 // https://medium.com/jsunderthescope/a-recursive-binary-search-in-javascript-b29efaff64d6
 // O(log n) ( the best sort, will be nlogn)
 
-const binarySearch = function (arr, num) {
+const binarySearch = (arr, num) => {
   if (arr.length === 0) return -1;
   if (arr.length === 1) return arr[0] === num;
   if (arr[0] > num) return -1;
   if (arr[arr.length - 1] < num) return -1;
 
   let start = 0;
-  let end = arr.length - 1;
-  let mid = (start + end) >> 1; // integer
-
-  while (start <= end) { // NOT start < end, need start <= end,
-    //let mid = (start + end) >> 1; // more space, put outside same some space
-    //console.log('start = ', start, 'end = ', end, 'mid=', mid)
-    if (arr[mid] === num) return mid;
+  let end = arr.length - 1
+  while (start <= end) { // need <=, not <
+    let mid = (start + end) >> 1;
+    if (arr[mid] === num) return 1;
     arr[mid] > num ? end = mid - 1 : start = mid + 1;
-    mid = (start + end) >> 1;
   }
-
   return -1;
 }
-console.log(binarySearch([-1], 5));
-console.log(binarySearch([1, 3, 5], 5));
-console.log(binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
 
-
-
-// ordered array -- check mergeSort
 const binarySearchRecursive = (arr, num, start = 0, end = arr.length - 1) => {
   if (arr.length === 0) return -1;
   if (arr.length === 1) return arr[0] === num;
   if (arr[start] > num) return -1;
   if (arr[end] < num) return -1;
+  if (start > end) return -1;
 
-  while (start <= end) {
-    let mid = (start + end) >> 1;
-    if (arr[mid] === num) return 1;
-    arr[mid] > num ?
-      binarySearchRecursive(arr.slice(0, mid), num, start, end = mid - 1) :
-      binarySearchRecursive(arr.slice(mid + 1), num, start = mid + 1, end)
-  }
-  return -1;
+  let mid = (start + end) >> 1;
+
+  if (arr[mid] === num) return 1;
+  return arr[mid] > num ?
+    binarySearchRecursive(arr, num, start, end = mid - 1) :
+    binarySearchRecursive(arr, num, start = mid + 1, end)
 }
+
+console.log('binarySearch', binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
 console.log('binarySearchRecursive', binarySearchRecursive([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
 
 const recursiveFunction = function (arr, x, start, end) {
   if (arr.length === 0) return false;
-
-  // Base Condition 
   if (start > end) return false;
-
-  // Find the middle index 
-  let mid = (start + end) >> 1; //Math.floor((start + end) / 2);
-
-  // Compare mid with given key x 
-  if (arr[mid] === x) return true;
-
-  // If element at mid is greater than x, 
-  // search in the left half of mid 
-  if (arr[mid] > x)
-    return recursiveFunction(arr, x, start, mid - 1);
-  else
-
-    // If element at mid is smaller than x, 
-    // search in the right half of mid 
-    return recursiveFunction(arr, x, mid + 1, end);
-}
-
-
-function binarySearch(arr, elem) {
-  if (arr.length === 0) return -1;
-  if (arr[0] > elem) return -1;
-  if (arr[arr.length - 1] < elem) return -1;
-
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = (start + end) >> 1; //Math.floor((start + end) / 2);
-
-  while (arr[middle] !== elem && start <= end) { //<=
-    elem < arr[middle] ? end = middle - 1 : start = middle + 1;
-    middle = Math.floor((start + end) / 2);
-  }
-
-  if (arr[middle] === elem) {
-    return middle;
-  }
-
-  return -1;
-}
-
-// Refactored Version
-function binarySearch(arr, num) {
-  if (arr.length === 0) return -1;
-  if (arr[0] > num) return -1;
-  if (arr[arr.length - 1] < num) return -1;
-
-  let start = 0;
-  let end = arr.length - 1;
   let mid = (start + end) >> 1;
-
-  while (start <= end) { //<=
-    if (arr[mid] === num) return mid;
-    arr[mid] > num ? mid = end-- : mid = start++;
-    mid = (start + end) >> 1;
-  }
-  return arr[mid] === num ? mid : -1;
+  if (arr[mid] === x) return true;
+  return arr[mid] > x ?
+    recursiveFunction(arr, x, start, mid - 1) : recursiveFunction(arr, x, mid + 1, end);
 }
 
-//[1, 2, 3, 4, 5] mid = (0 + 4) / 2 = 2
-//[1, 2, 3, 4, 5] mid = (5) / 2 = 2.5 >> 1
-const binarySearch = function (arr, num) {
-  if (arr.length === 0) return -1;
-  if (arr[0] > num) return -1;
-  if (arr[arr.length - 1] < num) return -1;
 
-  let start = 0;
-  let end = arr.length - 1;
 
-  while (start <= end) { // NOT start < end, need start <= end,
-    let mid = (start + end) >> 1;
-
-    if (arr[mid] === num) return mid;
-    if (arr[mid] > num) {
-      end = mid - 1;
-    } else start = mid + 1;
-  }
-
-  return -1;
-}
-console.log(binarySearch([-1], 5));
-console.log(binarySearch([1, 3, 5], 5));
-console.log(binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
-
-function binarySearch(arr, elem) {
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== elem && start <= end) {
-    if (elem < arr[middle]) end = middle - 1;
-    else start = middle + 1;
-    middle = Math.floor((start + end) / 2);
-  }
-  return arr[middle] === elem ? middle : -1;
-}
-
-// Refactored Version
-function binarySearch(arr, elem) {
-  if (arr.length === 0) return -1;
-
-  var start = 0;
-  var end = arr.length - 1;
-  var mid = (start + end) >> 1;
-
-  while (arr[mid] !== elem && start <= end) {
-    elem < arr[mid] ? end = mid - 1 : start = mid + 1;
-    mid = (start + end) >> 1;
-  }
-
-  return arr[mid] === elem ? mid : -1;
-}
-
-// ordered array
-// use two pointers
-const binarySearch = function (arr, num) {
-  if (arr.length === 0) return -1;
-  if (arr.length === 1) return arr[0] === num;
-
-  let start = 0;
-  let end = arr.length - 1;
-  let mid = Math.floor((start + end) / 2); // let mid = parseInt(arr.length / 2);
-
-  while (start <= end) {
-    if (arr[mid] === num) return true;
-    arr[mid] > num ? mid = end-- : mid = start++;
-    mid = Math.floor((start + end) / 2)
-  }
-
-  return false;
-}
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // true
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // true
-console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // false
 
 
 
--- Data-Structure/JS/searching/test.js
+++ Data-Structure/JS/searching/test.js
@@ -10,45 +10,25 @@ const binarySearch = (arr, num) => {
     let end = arr.length - 1
     while (start <= end) { // need <=, not <
         let mid = (start + end) >> 1;
-        console.log('arr[mid]=', arr[mid]);
         if (arr[mid] === num) return 1;
         arr[mid] > num ? end = mid - 1 : start = mid + 1;
     }
     return -1;
 }
-// console.log('binarySearch', binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
-// console.log('binarySearch', binarySearch([1, 2, 4, 5], 2)); // 1
-// console.log('binarySearch', binarySearch([1, 2, 4, 5], 0)); // -1
-// console.log('binarySearch', binarySearch([1, 2, 3, 4, 5], 3)); // 1
-const binarySearchRecursive1 = function (arr, num, start = 0, end = arr.length - 1) {
-    if (arr.length === 0) return -1;
-    if (arr[0] > num) return -1;
-    if (arr[arr.length - 1] < num) return -1;
 
-    while (start <= end) {
-        let mid = (start + end) >> 1;
-        if (arr[mid] === num) return 1;
-        arr[mid] < num ? binarySearchRecursive(arr, num, start = mid + 1, end)
-            : binarySearchRecursive(arr, num, start, end = mid - 1);
-    }
-    return -1;
-}
 const binarySearchRecursive = (arr, num, start = 0, end = arr.length - 1) => {
     if (arr.length === 0) return -1;
     if (arr.length === 1) return arr[0] === num;
     if (arr[start] > num) return -1;
     if (arr[end] < num) return -1;
+    if (start > end) return -1;
 
-    while (start <= end) {
-        let mid = (start + end) >> 1;
-        if (arr[mid] === num) return 1;
-        arr[mid] > num ?
-            binarySearchRecursive(arr.slice(0, mid), num, start, end = mid - 1) :
-            binarySearchRecursive(arr.slice(mid + 1), num, start = mid + 1, end)
-    }
-    return -1;
+    let mid = (start + end) >> 1;
+
+    if (arr[mid] === num) return 1;
+    return arr[mid] > num ?
+        binarySearchRecursive(arr, num, start, end = mid - 1) :
+        binarySearchRecursive(arr, num, start = mid + 1, end)
 }
+
 console.log('binarySearchRecursive', binarySearchRecursive([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
-// console.log('binarySearchRecursive', binarySearchRecursive([1, 2, 4, 5], 2)); // 1
-// console.log('binarySearchRecursive', binarySearchRecursive([1, 2, 4, 5], 0)); // -1
-// console.log('binarySearchRecursive', binarySearchRecursive([1, 2, 3, 4, 5], 3)); // 1
