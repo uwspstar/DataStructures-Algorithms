e53821a5622ca2358a80a82dd3e12691bd5db63b
xing wang <uwspstar@gmail.com>
2020-07-27 15:45:33 -0500

    binary search

--- Data-Structure/JS/searching/Binary-Search.js
+++ Data-Structure/JS/searching/Binary-Search.js
@@ -29,6 +29,22 @@ console.log(binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
 
 
 // ordered array -- check mergeSort
+const binarySearchRecursive = (arr, num, start = 0, end = arr.length - 1) => {
+  if (arr.length === 0) return -1;
+  if (arr.length === 1) return arr[0] === num;
+  if (arr[start] > num) return -1;
+  if (arr[end] < num) return -1;
+  
+  while (start < end) {
+    let mid = (start + end) >> 1;
+    if (arr[mid] === num) return 1;
+    (arr[mid] > num) ?
+      binarySearchRecursive(arr.slice(0, mid), num, start, mid - 1) :
+      binarySearchRecursive(arr.slice(mid + 1), num, mid + 1, end)
+
+  }
+  return -1;
+}
 const binarySearchRecursive = function (arr, num, start = 0, end = arr.length - 1) {
   if (arr.length === 0) return -1;
   if (arr[0] > num) return -1;
