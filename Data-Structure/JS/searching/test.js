9fcda04b3026614c2939691de37061480874c7bc
xing wang <uwspstar@gmail.com>
2020-07-27 15:13:47 -0500

    sort

--- Data-Structure/JS/searching/test.js
+++ Data-Structure/JS/searching/test.js
@@ -3,85 +3,13 @@
 const binarySearchRecursive = function (arr, num, start = 0, end = arr.length - 1) {
     if (arr.length === 0) return -1;
     if (start <= end) {
-        let mid = (start + end) >> 1; 
+        let mid = (start + end) >> 1;
         if (arr[mid] === num) return 1;
         arr[mid] < num ? binarySearchRecursive(arr, num, mid + 1, end)
             : binarySearchRecursive(arr, num, start, mid - 1);
-    } 
+    }
     return -1;
 }
-console.log(binarySearchRecursive([1, 2, 4, 5], 2));
-console.log(binarySearchRecursive([1, 2, 4, 5], 0));
-console.log(binarySearchRecursive([1, 2, 3, 4, 5], 3));
-/*
-console.log(5 >> 1) // 5 / 2 ** 1
-console.log(Math.floor(5 / 2)) // 5 / 2 ** 1
-console.log(4 / 2) // 5 / 2 ** 1
-
-const searchMatchStr = function (str, word) {
-    if (str.length < word.length) return -1;
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < word.length; j++) {
-            if (word[j] !== str[i + j]) break;
-            if (j === word.length - 1) count++;
-        }
-    }
-    return count;
-}
-console.log(searchMatchStr("lorie loled", "ll"));
-console.log(searchMatchStr("lorie loled", "l"));
-console.log(searchMatchStr("lorie loled", "lo"));
-console.log(searchMatchStr("lorie loled", "lol"));
-
-const findMissingNum = function (arr) {
-    if (arr.length === 0) return -1;
-    //1+2+3+...+n = n/2 * (1+n)
-    let n = arr.length + 1;
-    let sum = n * (1 + n) >> 1 // middle
-    //console.log('n=', n, 'sum=', sum)
-    let total = 0;
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i];
-    }
-    //console.log('n=', n, 'sum=', sum, 'total=', total)
-    return sum - total;
-}
-console.log(findMissingNum([1, 2, 4, 5]))
-console.log(findMissingNum([1, 2, 3, 4, 6, 7, 8]))
-
-// ("lorie loled", "lol")
-const searchMathStr = function (long, short) {
-    let count = 0;
-    for (let i = 0; i < long.length; i++) {
-        for (let j = 0; j < short.length; j++) {
-            console.log('i=', i, 'j=', j, 'short[j]=', short[j], 'long[i + j]=', long[i + j])
-            if (short[j] !== long[i + j]) break;
-            if (j === short.length - 1) count++;// find one math increase
-            console.log('count =', count)
-        }
-    }
-    return count;
-}
-console.log(searchMathStr("lorie loled", "lol")) // 1
-console.log(searchMathStr("lorie loled", "ri")) // 1
-console.log(searchMathStr("lorie loled", "l")) // 3
-
-
-
-function binarySearch(arr, num) {
-    let start = 0
-    let end = arr.length - 1
-    let mid = (start + end) >> 1
-    while (start <= end) {
-        if (arr[mid] === num) return mid
-        arr[mid] > num ? mid = end-- : mid = start++
-        mid = (start + end) >> 1
-    }
-    return arr[mid] === num ? mid : -1;
-}
-
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13))
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103))
-
-*/
+console.log(binarySearchRecursive([1, 2, 4, 5], 2)); // 1
+console.log(binarySearchRecursive([1, 2, 4, 5], 0)); // -1
+console.log(binarySearchRecursive([1, 2, 3, 4, 5], 3)); // 1
--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -1,17 +1,16 @@
 // bubbleSort : out loop i--; inner loop j++; swap
-// selectionSort : out loop i++; inner loop j++; smallIdx; swap
-// insertingSort : left side sorted, out loop i++, i = 1;  inner loop j--, j= i-1
+// selectionSort : out loop i++; inner loop j++; smallIdx; swap; each loop find smallest one, and put on left
+// insertingSort : start first item, left side sorted, out loop i++, i = 1;  inner loop j--, start j= i-1
 // mergeSort : merge(left,right), sort(arr), no swap
-// quickSort : pivot, swap
+// quickSort : pivot, swap, each step swap current index and pi
 
-const swap = (arr, i, j) => {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-}
+const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 
 const quickSortHelp = (arr) => {
+
     if (arr.length < 2) return arr;
 
-    const getPivotIndex = (arr, low, high) => {
+    const getPivotIndex = (arr, low = 0, high = arr.length - 1) => {
         let pi = low;
         let pivot = arr[high];
         for (let i = low; i <= high; i++) {
@@ -20,10 +19,9 @@ const quickSortHelp = (arr) => {
                 pi++;
             }
         }
-        swap(arr, high, pi)
+        swap(arr, pi, high)
         return pi;
     }
-
     const quickSort = (arr, low = 0, high = arr.length - 1) => {
         if (arr.length < 2) return arr;
         if (low < high) {
@@ -37,18 +35,18 @@ const quickSortHelp = (arr) => {
 }
 const mergeSortHelp = (arr) => {
     if (arr.length < 2) return arr;
-
     const merge = (left, right) => {
         if (left.length === 0) return right;
-        if (left.right === 0) return left;
+        if (right.length === 0) return left;
         let result = [];
         while (left.length > 0 && right.length > 0) {
-            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
+            left[0] < right[0] ?
+                result.push(left.shift()) : result.push(right.shift());
         }
-        return result.concat(left, right);
+        return result.concat(left, right)
     }
     const mergeSort = (arr) => {
-        if (arr.length < 2) return arr; //base class
+        if (arr.length < 2) return arr;
         let mid = arr.length >> 1;
         let left = mergeSort(arr.slice(0, mid));
         let right = mergeSort(arr.slice(mid));
@@ -60,11 +58,11 @@ const mergeSortHelp = (arr) => {
 const insertionSort = (arr) => {
     if (arr.length < 2) return arr;
     for (let i = 1; i < arr.length; i++) {
-        let j = i - 1;
         let current = arr[i];
+        let j = i - 1;
         for (; j >= 0; j--) {
             if (arr[j] > current) {
-                arr[j + 1] = arr[j];
+                arr[j + 1] = arr[j]
             } else break;
         }
         arr[j + 1] = current;
@@ -74,13 +72,13 @@ const insertionSort = (arr) => {
 const selectionSort = (arr) => {
     if (arr.length < 2) return arr;
     for (let i = 0; i < arr.length; i++) {
-        let smallIndex = i;
-        for (let j = i; j < arr.length; j++) {
-            if (arr[j] < arr[smallIndex]) {
-                smallIndex = j;
+        let smIdx = i; //each loop find smallest one, and put on left
+        for (let j = smIdx; j < arr.length; j++) {
+            if (arr[j] < arr[smIdx]) {
+                smIdx = j;
             }
         }
-        if (smallIndex !== i) swap(arr, i, smallIndex);
+        if (smIdx !== i) swap(arr, i, smIdx);
     }
     return arr;
 }
@@ -89,12 +87,17 @@ const bubbleSort = (arr) => {
     for (let i = arr.length - 1; i >= 0; i--) {
         let isSwap = false;
         for (let j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) swap(arr, i, j);
-            isSwap = true;
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                isSwap = true;
+            }
         }
         if (!isSwap) break;
     }
     return arr;
 }
-
-console.log(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log('quickSortHelp', quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log('mergeSortHelp', mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log('insertionSort', insertionSort([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log('selectionSort', selectionSort([8, 1, 2, 3, 4, 5, 6, 7]));
+console.log('bubbleSort', bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
