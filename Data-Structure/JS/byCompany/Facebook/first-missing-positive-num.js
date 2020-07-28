ef794e0c644f71067a9dfc01e67ffca0485eee95
xing wang <uwspstar@gmail.com>
2020-07-28 16:47:22 -0500

    daily

--- Data-Structure/JS/byCompany/Facebook/first-missing-positive-num.js
+++ Data-Structure/JS/byCompany/Facebook/first-missing-positive-num.js
@@ -5,8 +5,12 @@ For example, the input [3, 4, -1, 1] should return 2 because it is the smallest 
 
 Your solution should run in "linear time" and use "constant space".
  
+A naive solution is to first sort the array, while removing negative numbers. Then, simply iterate over the array and return the first element that doesn't match its index. However, the time complexity of this solution is O(n*log(n)) (sorting complexity for mergeSort or heapSort, or quickSort on average).
 */
 const firstMissingInteger = function (arr) {
     if (arr.length === 0) return undefined;
     // [3, 4, -1, 1]
+    for (let i = 0; i < arr.length; i++) {
+        
+    }
 }
