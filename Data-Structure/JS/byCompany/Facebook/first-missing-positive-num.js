a766a69441b98915be13996436624cd3645340b5
xing wang <uwspstar@gmail.com>
2020-07-28 17:15:40 -0500

    daily

--- Data-Structure/JS/byCompany/Facebook/first-missing-positive-num.js
+++ Data-Structure/JS/byCompany/Facebook/first-missing-positive-num.js
@@ -9,8 +9,16 @@ A naive solution is to first sort the array. However, the time complexity of thi
 */
 const firstMissingInteger = function (arr) {
     if (arr.length === 0) return undefined;
-    // [3, 4, -1, 1]
-    for (let i = 0; i < arr.length; i++) {
-
-    }
 }
+
+
+// extra space
+const firstMissingInteger = function (arr) {
+    if (arr.length === 0) return undefined;
+    let set = new Set(arr);
+    for (let i = 1; i < arr.length; i++) {
+        if (!set.has(i)) return i;
+    }
+    return undefined;
+}
+console.log(firstMissingInteger([5, 3, 4, -1, -2, -3, 1]));
