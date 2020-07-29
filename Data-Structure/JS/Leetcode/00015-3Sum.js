fa9a95f38c14a6dd4af8177c046c7a845ab2cc8a
xing wang <uwspstar@gmail.com>
2020-07-29 10:11:50 -0500

    daily

--- Data-Structure/JS/Leetcode/00015-3Sum.js
+++ Data-Structure/JS/Leetcode/00015-3Sum.js
@@ -3,7 +3,7 @@
 
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
-The solution set must not contain duplicate triplets.
+The solution set must "not" contain "duplicate triplets".
 
 Given array nums = [-1, 0, 1, 2, -1, -4],
 
@@ -14,8 +14,24 @@ A solution set is:
 ]
 */
 
+// 1: naive solution 3 nest loop (N^3)
+// 2: a + b = 0 - c = -c
+
+// slow, how to remove duplicate ?
 const threeSumZero = function (arr) {
     if (arr.length < 3) return undefined;
-    let result = [];
-    return result;
+
+    let set = new Set();
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            for (let x = j + 1; x < arr.length; x++) {
+                if (arr[i] + arr[j] + arr[x] === 0) {
+                    set.add([arr[i], arr[j], arr[x]]);
+                }
+            }
+        }
+    }
+    return set;
 }
+
+console.log(threeSumZero([-3, -1, 0, 1, 2, -1, -4]))
