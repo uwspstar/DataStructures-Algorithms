3fd36efc9d1f618f70c78cc654567d38080304df
xing wang <uwspstar@gmail.com>
2020-07-31 10:41:09 -0500

    fib

--- Data-Structure/JS/Top100/00001-fibonacci-recursive.js
+++ Data-Structure/JS/Top100/00001-fibonacci-recursive.js
@@ -1,4 +1,21 @@
+// think about how to save space ,
+// https://medium.com/free-code-camp/an-intro-to-algorithms-dynamic-programming-dd00873362bb 
+
+// O(n) time | O(1) space
+const fib = (n) => {
+    const lastTwo = [0, 1];
+    let counter = 3;
+    while (count <= n) {
+        const nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        count++;
+    }
+    return n > 1 ? lastTwo[1] : lastTwo[0]
+}
+
 // 1 : regular loop
+// time : O(N), space: O(N)
 const fibonacci = function (n) {
     if (n <= 2) return 1;
     let result = [1, 1];
@@ -23,6 +40,7 @@ const fibonacci = function (n) {
 console.log(fibonacci(40));//102334155
 console.log(fibonacci(1000));//4.346655768693743e+208  very fast
 
+// 3: recursive , no memo
 const fibonacci2 = function (n) {
     if (n < 3) return 1;
     return result = fibonacci2(n - 1) + fibonacci2(n - 2);
@@ -32,3 +50,4 @@ const fibonacci2 = function (n) {
 console.log(fibonacci2(45)); // 1134903170  very slow
 
 console.log(fibonacciRecursive(7)); // [1, 1, 2, 3, 5, 8, 13]
+
