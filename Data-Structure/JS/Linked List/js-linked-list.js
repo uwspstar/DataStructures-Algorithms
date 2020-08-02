c8a6b1022bbe84e907de1c2e31584c62100d2062
xing wang <uwspstar@gmail.com>
2020-08-02 06:47:58 -0500

    likedlist

--- Data-Structure/JS/Linked List/js-linked-list.js
+++ Data-Structure/JS/Linked List/js-linked-list.js
@@ -12,7 +12,9 @@ class LinkedList {
     constructor(data) {
         this.head = new Node(data);
     }
-
+    getHead() {
+        return this.head === null ? null : this.head;
+    }
     append(data) { // the tail :  the current.next === null;
         if (this.head === null) {
             this.head = new Node(data);
@@ -25,15 +27,42 @@ class LinkedList {
         }
         return this;
     }
-
-    head() {
-        return this.head === null ? null : this.head;
+    insert(data) {
+        this.insertAt(data, 0);
     }
+    insertAt(data, index) {
+        const node = new Node(data);
+        let current = this.head; 
+        if (index === 0) {
+            this.head = node;
+            node.next = current;
+        } else {
+            let count = 0;
+            let prev = null;
+            //insertAt(4, 1)
+            // 5-->-->3-->2
+            while (count < index && current.next) {
+                count++;
+                prev = current;
+                current = current.next;
+            }
+            node.next = current;
+            prev.next = node;  
+        }
+        return this;
+
+    }
+
+
 }
 
-let list = new LinkedList(5)
-list.append(4)
-list.append(3)
-list.append(2)
-
+let list = new LinkedList(6);
+list.append(5);
+list.append(4);
+list.append(2); //6->5->4->2
+list.insert(10);//10->6->5->4->2
 console.log(JSON.stringify(list));
+list.insert('a');
+list.insertAt('c', 3);
+console.log(JSON.stringify(list));
+console.log(JSON.stringify(list.getHead()));
