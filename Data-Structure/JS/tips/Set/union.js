6c210d3c884b8b10267fe13e9725ea800a342a9c
xing wang <uwspstar@gmail.com>
2020-08-03 21:08:59 -0500

    daily

--- Data-Structure/JS/tips/Set/union.js
+++ Data-Structure/JS/tips/Set/union.js
@@ -1,4 +1,5 @@
 let a = new Set([1, 2, 3, 4]);
 let b = new Set([3, 4, 5])
 let union = new Set([...a].concat([...b]))
-console.log(a, b, union);
+let union2 = new Set([...a, ...b])
+console.log(a, b, union, union2);
