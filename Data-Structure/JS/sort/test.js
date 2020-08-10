3b2cd1ec9733604905be45781e1bed2ae37f391e
xing wang <uwspstar@gmail.com>
2020-08-10 06:56:00 -0500

    daily

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -101,9 +101,12 @@ const bubbleSort = (arr) => {
     for (let i = arr.length - 1; i >= 0; i--) {
         let isSwap = false;
         for (let j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
-            
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                isSwap = true;
+            }
         }
+        if (!isSwap) break;
     }
     return arr;
 }
