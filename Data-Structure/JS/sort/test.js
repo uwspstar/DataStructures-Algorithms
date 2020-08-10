e82011889cedf43f227e37886638d4751610c1c3
xing wang <uwspstar@gmail.com>
2020-08-10 06:54:15 -0500

    daily

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -82,7 +82,7 @@ const selectionSort = (arr) => {
     }
     return arr;
 }
-const bubbleSort = (arr) => {
+const bubbleSort1 = (arr) => {
     if (arr.length < 2) return arr;
     for (let i = arr.length - 1; i >= 0; i--) {
         let isSwap = false;
@@ -96,7 +96,17 @@ const bubbleSort = (arr) => {
     }
     return arr;
 }
-
+const bubbleSort = (arr) => {
+    if (arr.length < 2) return arr;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let isSwap = false;
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+            
+        }
+    }
+    return arr;
+}
 console.log('quickSortHelp', quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log('mergeSortHelp', mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log('insertionSort', insertionSort([8, 1, 2, 3, 4, 5, 6, 7]));
