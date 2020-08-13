1cef25df40287119dcd9ad6ec157d3b29011b369
xing wang <uwspstar@gmail.com>
2020-08-13 07:13:06 -0500

    daily

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -75,6 +75,21 @@ const insertionSort = (arr) => {
         let current = arr[i];
         let j = i - 1;
         for (; j >= 0; j--) {
+            if (arr[j] < current) {
+                arr[j + 1] = arr[j];
+            }
+            else break;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+const insertionSort1 = (arr) => {
+    if (arr.length < 2) return arr;
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        for (; j >= 0; j--) {
             if (arr[j] > current) {
                 arr[j + 1] = arr[j]
             } else break;
