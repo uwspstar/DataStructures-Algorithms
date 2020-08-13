3780fe256cdf98c3329360473340278068890ee1
xing wang <uwspstar@gmail.com>
2020-08-13 07:32:11 -0500

    daily

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -33,7 +33,7 @@ const quickSortHelp = (arr) => {
     }
     return quickSort(arr);
 }
-const mergeSortHelp = (arr) => {
+const mergeSortHelp1 = (arr) => {
     if (arr.length < 2) return arr;
     const merge = (left, right) => {
         if (left.length === 0) return right;
@@ -55,19 +55,26 @@ const mergeSortHelp = (arr) => {
     }
     return mergeSort(arr)
 }
-const insertionSort1 = (arr) => {
+const mergeSortHelp = (arr) => {
     if (arr.length < 2) return arr;
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        for (; j >= 0; j--) {
-            if (arr[j] > current) {
-                arr[j + 1] = arr[j];
-            } else break;
+    const merge = (left, right) => {
+        if (left.length === 0) return right;
+        if (right.length === 0) return left;
+        let result = [];
+        while (left.length > 0 && right.length > 0) {
+            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
         }
-        arr[j + 1] = current;
+        return result.concat(left, right);
     }
-    return arr;
+    const mergeSort = (arr) => {
+        if (arr.length < 2) return arr; // base case;
+        let mid = arr.length >> 1;
+        let left = mergeSort(arr.slice(0, mid));
+        let right = mergeSort(arr.slice(mid));
+        return merge(left, right);
+
+    }
+    return mergeSort(arr);
 }
 const insertionSort = (arr) => {
     if (arr.length < 2) return arr;
@@ -75,7 +82,7 @@ const insertionSort = (arr) => {
         let current = arr[i];
         let j = i - 1;
         for (; j >= 0; j--) {
-            if (arr[j] < current) {
+            if (arr[j] > current) {
                 arr[j + 1] = arr[j];
             }
             else break;
@@ -83,21 +90,6 @@ const insertionSort = (arr) => {
         arr[j + 1] = current;
     }
     return arr;
-}
-const insertionSort1 = (arr) => {
-    if (arr.length < 2) return arr;
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        for (; j >= 0; j--) {
-            if (arr[j] > current) {
-                arr[j + 1] = arr[j]
-            } else break;
-        }
-        arr[j + 1] = current;
-    }
-    return arr;
-
 }
 const selectionSort = (arr) => {
     if (arr.length < 2) return arr;
