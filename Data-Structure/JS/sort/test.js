25e7456b25053206fc491f2075fdbf509fede993
xing wang <uwspstar@gmail.com>
2020-08-16 06:35:51 -0500

    daily sort

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -70,7 +70,7 @@ const insertionSort = (arr) => {
     }
     return arr;
 }
-const selectionSort = (arr) => {
+const selectionSort1 = (arr) => {
     if (arr.length < 2) return arr;
     for (let i = 0; i < arr.length; i++) {
         let smallIndex = i;
@@ -83,18 +83,17 @@ const selectionSort = (arr) => {
     }
     return arr;
 }
-const bubbleSort1 = (arr) => {
+const selectionSort = (arr) => {
     if (arr.length < 2) return arr;
-    for (let i = arr.length - 1; i >= 0; i--) {
-        let isSwap = false;
-        for (let j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-                isSwap = true;
+    for (let i = 0; i < arr.length; i++) {
+        let smallIndex = i;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[smallIndex]) {
+                smallIndex = j;
             }
         }
-        if (!isSwap) break;
-    }
+        if (smallIndex !== i) swap(arr, smallIndex, i);
+    } 
     return arr;
 }
 const bubbleSort = (arr) => {
@@ -111,7 +110,6 @@ const bubbleSort = (arr) => {
     }
     return arr;
 }
-
 console.log('quickSortHelp', quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log('mergeSortHelp', mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log('insertionSort', insertionSort([8, 1, 2, 3, 4, 5, 6, 7]));
