4b6720c711675bc838ac5a9a70146ab865829eae
xing wang <uwspstar@gmail.com>
2020-08-19 15:01:30 -0500

    python

--- Data-Structure/Python/Practice/a_b_c.py
+++ Data-Structure/Python/Practice/a_b_c.py
@@ -8,12 +8,26 @@ a^2 + b^2 = c^2
 import time
 
 
-def slowMode():
-
+def fastMode():
     start_time = time.time()
-    for a in range(0, 1001):
-        for b in range(0, 1001):
-            for c in range(0, 1001):
+    for a in range(0, 1000):
+        for b in range(0, 1000):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                print('a=', a, 'b=', b, 'c=', c)
+    end_time = time.time()
+    print('time', (end_time - start_time))
+    print('----- done -----')
+
+
+print('fastMode : ', fastMode())
+
+
+def slowMode():
+    start_time = time.time()
+    for a in range(0, 1000):
+        for b in range(0, 1000):
+            for c in range(0, 1000):
                 if a + b + c == 1000 and a**2 + b**2 == c**2:
                     print('a=', a, 'b=', b, 'c=', c)
     end_time = time.time()
@@ -21,4 +35,4 @@ def slowMode():
     print('----- done -----')
 
 
-print('slowMode : ', slowMode())
+# print('slowMode : ', slowMode())
