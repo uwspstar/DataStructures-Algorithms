f310fee5c5d41cc0deebea4bfd69db2b6866e537
xing wang <uwspstar@gmail.com>
2020-08-21 06:31:30 -0500

    daily sort

--- Data-Structure/JS/searching/Binary-Search.js
+++ Data-Structure/JS/searching/Binary-Search.js
@@ -4,10 +4,12 @@
 // https://medium.com/jsunderthescope/a-recursive-binary-search-in-javascript-b29efaff64d6
 // O(log n) ( the best sort, will be nlogn)
 
-const binarySearch = (arr, num) => {
+const binarySearch1 = (arr, num) => {
+
   if (arr.length === 0) return -1;
   if (arr.length === 1) return arr[0] === num;
-  if (arr[0] > num) return -1;
+
+  if (arr[0] > num) return -1; //binary ordered 
   if (arr[arr.length - 1] < num) return -1;
 
   let start = 0;
@@ -19,12 +21,26 @@ const binarySearch = (arr, num) => {
   }
   return -1;
 }
+const binarySearch = (arr, num) => {
+  if (arr.length === 0) return -1;
+  if (arr.length === 1) return arr[0] === num;
+  if (arr[0] > num) return -1;
+  if (arr[arr.length - 1] < num) return -1;
 
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = (start + end) >> 1;
+    if (arr[mid] === num) return 1;
+    arr[mid] > num ? end = mid - 1 : start = mid + 1;
+  }
+  return -1;
+}
 
 const binarySearchRecursive = (arr, num, start = 0, end = arr.length - 1) => {
   if (arr.length === 0) return -1;
   if (arr.length === 1) return arr[0] === num;
-  
+
   if (arr[start] > num) return -1;
   if (arr[end] < num) return -1;
   if (start > end) return -1;
@@ -37,9 +53,6 @@ const binarySearchRecursive = (arr, num, start = 0, end = arr.length - 1) => {
     binarySearchRecursive(arr, num, start = mid + 1, end)
 }
 
-console.log('binarySearch', binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
-console.log('binarySearchRecursive', binarySearchRecursive([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
-
 const recursiveFunction = function (arr, x, start, end) {
   if (arr.length === 0) return false;
   if (start > end) return false;
@@ -50,6 +63,8 @@ const recursiveFunction = function (arr, x, start, end) {
 }
 
 
+console.log('binarySearch', binarySearch([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], -5));
+//console.log('binarySearchRecursive', binarySearchRecursive([-1, 0, 1, 3, 5, 6, 7, 10, 20, 30, 50], 5));
 
 
 
