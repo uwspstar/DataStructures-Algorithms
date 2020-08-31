00750d3c1b381fae67feb7a33d63e177c842c0b2
xing wang <uwspstar@gmail.com>
2020-08-31 06:05:25 -0500

    daily question

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -35,7 +35,7 @@ const quickSortHelp = (arr) => {
     }
     return quickSort(arr);
 }
-const mergeSortHelp = (arr) => {
+const mergeSortHelp1 = (arr) => {
     if (arr.length < 2) return arr;
     const merge = (left, right) => {
         if (left.length === 0) return right;
@@ -56,6 +56,9 @@ const mergeSortHelp = (arr) => {
     }
     return mergeSort(arr);
 }
+const mergeSortHelp = (arr) => {
+
+}
 const insertionSort1 = (arr) => {
     if (arr.length < 2) return arr;
     for (let i = 1; i < arr.length; i++) {
@@ -69,21 +72,6 @@ const insertionSort1 = (arr) => {
         }
         arr[j + 1] = current;
     }
-    return arr;
-}
-const insertionSort = (arr) => {
-    if (arr.length < 2) return arr;
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        for (; j >= 0; j--) {
-            if (arr[j] > current) {
-                arr[j + 1] = arr[j];
-            } else break;
-        }
-        arr[j + 1] = current;
-    }
-
     return arr;
 }
 const selectionSort = (arr) => {
