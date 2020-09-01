0accdf3370684916fbf50e148c42a7233586daaa
xing wang <uwspstar@gmail.com>
2020-09-01 06:04:00 -0500

    daily

--- Data-Structure/JS/Top100/00007-is-Palindrome.js
+++ Data-Structure/JS/Top100/00007-is-Palindrome.js
@@ -1,5 +1,5 @@
 // 1: recursive 
-// is it is integer, without using str function, how you do it ? ( see twitter)
+// if it is integer, without using str function, how you do it ? ( see twitter)
 
 const isPalindromeRecursive = function (str) {
     if (str.length < 2) return true;
@@ -7,10 +7,11 @@ const isPalindromeRecursive = function (str) {
     return str[0] === str[str.length - 1] ?
         isPalindromeRecursive(str.slice(1, -1)) : false;
 
-    // cannot use arr.shift().pop() 
+    // cannot use arr.shift().pop() chain, for the chain method need "next"
     // arr.shift() is one item
     // arr.pop() is one item
-    // A negative index can be used, indicating an offset from the end of the sequence. slice(1,-1) extracts the second element through the second-to-last element in the sequence. 
+    // A negative index can be used, indicating an offset from the end of the sequence. 
+    // slice(1,-1) extracts the second element through the second-to-last element in the sequence. 
     // slice(1,-1) remove last one and first one
     // string using slice(start[,end]) only
     // arr use arr.slice(), arr.shift(), arr.pop()
