e968a8471cfa96cfe2b9a9c5c4fb7f75b2b39714
xing wang <uwspstar@gmail.com>
2020-09-01 06:16:41 -0500

    daily

--- Data-Structure/JS/Top100/00007-is-Palindrome.js
+++ Data-Structure/JS/Top100/00007-is-Palindrome.js
@@ -1,7 +1,7 @@
 
 // if it is integer, without using str function, how you do it ? ( see twitter)
 
-// meet in middle : two pointers
+// 1 :  meet in middle : two pointers
 const isPalindrome = function (str) {
     if (str.length < 2) return true;
     let left = 0;
@@ -14,7 +14,7 @@ const isPalindrome = function (str) {
     return true;
 }
 
-// recursive 
+// 2: recursive 
 const isPalindromeRecursive = function (str) {
     if (str.length < 2) return true;
 
@@ -31,26 +31,23 @@ const isPalindromeRecursive = function (str) {
     // arr use arr.slice(), arr.shift(), arr.pop()
 }
 
-// covert str to arr, only arr has reverse function
-const isPalindromeReverse = function (str) {
-    if (str.length < 2) return true;
-    return str.split('').reverse().join('') === str;
-}
-
-const isPalindromeRecursive = function (str) {
+const isPalindromeRecursive2 = function (str) {
     if (str.length === 1) return true;
     if (str.length === 2) return str[0] === str[1];
     if (str.slice(0, 1) === str.slice(-1)) return isPalindromeRecursive(str.slice(1, -1));
     return false;
 }
 
-console.log(isPalindromeRecursive('tacocat'));
-console.log(isPalindromeRecursive('amanaplanacanalpanama'));
-console.log(isPalindromeRecursive('amanaplanacanalpandemonium'));
+// 3: covert str to arr, only arr has reverse function
+const isPalindromeReverse = function (str) {
+    if (str.length < 2) return true;
+    return str.split('').reverse().join('') === str;
+}
 
-// meet in middle : regular start, end 
+// fast, O(N)
+// 4: meet in middle : regular start, end 
 const isPalindromeMeetInMiddle = function (str) {
-    if (str.length === 1) return false;
+    if (str.length === 1) return true;
     let mid = parseInt(str.length / 2);
     for (let i = 0; i <= mid; i++) {
         if (str[i] !== str[str.length - 1 - i]) return false;
@@ -58,7 +55,9 @@ const isPalindromeMeetInMiddle = function (str) {
     return true;
 }
 
-
+console.log(isPalindromeRecursive('tacocat'));
+console.log(isPalindromeRecursive('amanaplanacanalpanama'));
+console.log(isPalindromeRecursive('amanaplanacanalpandemonium'));
 
 console.log(isPalindromeMeetInMiddle('tacocat')); //true
 console.log(isPalindromeMeetInMiddle('amanaplanacanalpanama'));//true
