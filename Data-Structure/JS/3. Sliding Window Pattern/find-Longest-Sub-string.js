5ccc0a33683e485f9458fb769f8fc9a3f6a36a03
xing wang <uwspstar@gmail.com>
2020-09-03 18:17:38 -0500

    daily

--- Data-Structure/JS/3. Sliding Window Pattern/find-Longest-Sub-string.js
+++ Data-Structure/JS/3. Sliding Window Pattern/find-Longest-Sub-string.js
@@ -5,7 +5,6 @@ https://medium.com/outco/how-to-solve-sliding-window-problems-28d67601a66
 instead of incrementing the slow pointer up, you simply move it up the fast pointer’s location and then keep moving the fast pointer up. It sort of “jumps” to the index of the fast pointer when a certain condition is met.
 // check linked list find kth last node
 
-
 -- Sliding Window - findLongestSubstring
 Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with "all distinct characters".
 
@@ -14,11 +13,10 @@ Time Complexity - O(n)
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 arr = arr.slice(start, end)
 include start , not include end
-
 */
 
 const findLongestSubstring = function (str) {
-    if (str.length === 0) return 0
+    if (str.length === 0) return null;
 
     let fast = 0;
     let maxLen = -Infinity;
@@ -39,35 +37,8 @@ const findLongestSubstring = function (str) {
 
     return maxLen === -Infinity ? 0 : maxLen;
 }
-
-// same logic
-const findLongestSubstring = function (str) {
-    if (str.length === 0) return 0
-
-    let start = 0;
-    let end = 0;
-    let maxLen = -Infinity;
-    let arr = [];
-
-    while (start < str.length) {
-        //'rithmschool'
-        let index = arr.indexOf(str[start]);
-        if (index > -1) { // find 
-            end = index;
-            arr = arr.slice(end + 1, start);
-        }
-
-        arr.push(str[start]);
-        maxLen = Math.max(maxLen, arr.length);
-
-        start++;
-
-    }
-
-    return maxLen === -Infinity ? 0 : maxLen;
-}
-
-function findLongestSubstring(str) {
+ 
+function findLongestSubstring1(str) {
     let longest = 0;
     let seen = {};
     let start = 0;
@@ -85,11 +56,10 @@ function findLongestSubstring(str) {
     return longest;
 }
 
-console.log(findLongestSubstring('rithmschool')); // 7
-
-console.log(findLongestSubstring('')); // 0
-console.log(findLongestSubstring('thisisawesome'));// 6
-console.log(findLongestSubstring('thecatinthehat')); // 7
-console.log(findLongestSubstring('bbbbbb')); // 1
-console.log(findLongestSubstring('longestsubstring')); // 8
-console.log(findLongestSubstring('thisishowwedoit')); // 6
+console.log('findLongestSubstring', findLongestSubstring('rithmschool')); // 7
+console.log('findLongestSubstring', findLongestSubstring('')); // 0
+console.log('findLongestSubstring', findLongestSubstring('thisisawesome'));// 6
+console.log('findLongestSubstring', findLongestSubstring('thecatinthehat')); // 7
+console.log('findLongestSubstring1', findLongestSubstring('bbbbbb')); // 1
+console.log('findLongestSubstring1', findLongestSubstring('longestsubstring')); // 8
+console.log('findLongestSubstring1', findLongestSubstring('thisishowwedoit')); // 6
