f4ddd2d5eba14d252b37a69f8de895b82875b587
xing wang <uwspstar@gmail.com>
2020-09-10 17:44:11 -0500

    daily sort

---
+++ Data-Structure/JS/byCompany/Microsoft/add-Two-Numbers.js
@@ -0,0 +1,27 @@
+/*
+You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+*/
+
+class Node {
+    constructor(vale) {
+        this.vale = value;
+        this.next = null;
+    }
+}
+class ListNode {
+    constructor(value) {
+        this.head = new Node(value);
+    }
+    add(value) {
+        if (this.head === null) {
+
+        }
+    }
+}
+
+
+
