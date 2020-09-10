0c958e3d4e45e3d2f2a7303001ea5595ec3ce35e
xing wang <uwspstar@gmail.com>
2020-09-10 07:41:33 -0500

    daily sort

---
+++ Data-Structure/JS/Reverse/reverse-a-linked-list.js
@@ -0,0 +1,51 @@
+// https://wsvincent.com/javascript-reverse-linked-list/
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor(val) {
+        this.head = new Node(val);
+    }
+    append(val) {
+        let node = new Node(val);
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        return this;
+    }
+    // 1-->2-->3-->4
+    // c   n   
+    reverse() {
+        let current = this.head;
+        let pre = null;
+        let next = null;
+        //5->4->3->2->1
+        while (current !== null) {
+            next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        this.head = pre; // very important
+        return this;
+    }
+}
+
+let linkedList = new LinkedList(5);
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.append(40);
+console.log(JSON.stringify(linkedList));
+console.log(JSON.stringify(linkedList.reverse()));
+
