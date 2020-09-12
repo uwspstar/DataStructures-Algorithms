4aff7dd30f46637e1d88f3f926178078c9f15a0d
xing wang <uwspstar@gmail.com>
2020-09-12 07:53:14 -0500

    ip-address

--- Data-Structure/JS/Reverse/reverse-int.js
+++ Data-Structure/JS/Reverse/reverse-int.js
@@ -11,7 +11,7 @@ We can treat the input number like a stack, pop each least significant digit out
 The time complexity is O(log n) for processing each digit, where n is the input number and the space complexity is O(n) for creating another number of the same size.
 */
 
-const reverseInteger = (num) => {
+const reverseInteger1 = (num) => {
     let negative = 1;
 
     if (num < 0) {
@@ -28,6 +28,24 @@ const reverseInteger = (num) => {
     }
 
     return negative * result
+}
+// think about stack
+const reverseInteger = (num) => {
+    if (num > -9 || num < 9) return num;
+
+    let negative = 1;
+    if (num < 0) {
+        num = (-1) * num;
+        negative = -1;
+    }
+    // 1234 % 10 = 4
+    //
+    let result = 0;
+    while (num > 0) {
+        result = result * 10 + num % 10;
+        num = parseInt(num / 10);
+    }
+    return result * negative;
 }
 
 console.log(reverseInteger(135)) // 531
---
+++ Data-Structure/JS/Reverse/stack.js
@@ -0,0 +1,1 @@
+// reverse : stack , first in last out
