abf1292d6046fc1d8fab1159a7a4152eab7facc6
xing wang <uwspstar@gmail.com>
2020-09-21 07:33:59 -0500

    daily

--- Data-Structure/JS/Top100/0000000All-In-One.js
+++ Data-Structure/JS/Top100/0000000All-In-One.js
@@ -1,5 +1,42 @@
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 
+const isPalindrome = function (str) {
+    if (str.length < 2) return true;
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+const isPalindrome = (str) => {
+    if (str.length < 2) return true;
+    let low = 0;
+    let high = str.length - 1;
+    while (low <= high) {
+        if (str[low] !== str[high]) return false;
+        low++;
+        high--;
+    }
+    return true;
+}
+const isBalance = (arr) => {
+    if (arr.length % 2 !== 0) return false;
+    let map = { "[": "]", "(": ")", "{": "}", }
+    let stock = [];
+    for (let i = 0; i < arr.length; i++) {
+        let key = arr[i];
+        if (map[key]) {
+            stock.push(arr[i]);
+        } else {
+            let last = stock.pop(); // not arr.pop()
+            if (key !== map[last]) return false;
+        }
+    }
+    return stock.length === 0;
+}
 const sortColor = (arr) => {
     if (arr.length < 2) return arr;
     let p0 = 0;
@@ -31,3 +68,5 @@ const fibonacci = function (n) {
 
 console.log('fibonacci', fibonacci(45)); //1, 1, 2, 3, 5,..., 1134903170
 console.log('sortColor', sortColor([0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1]));
+console.log('isBalance: ', isBalance("[{()}]"));//true
+console.log('',('amanaplanacanalpanama'))
