ae999f3eb7ca3fbb8d88246a3b035b44b11e0b6b
xing wang <uwspstar@gmail.com>
2020-09-22 10:42:06 -0500

    tree

---
+++ Data-Structure/JS/byCompany/Amazon/matrix-spiral-print.js
@@ -0,0 +1,13 @@
+/*
+Print out the clockwise spiral traversal of the 2D matrix.
+
+arr = [
+    [1,  2,  3,  4,  5],
+    [6,  7,  8,  9,  10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+]
+
+The clockwise spiral traversal of this array is:
+1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12
+*/
---
+++ Data-Structure/JS/byCompany/Apple/can-spell.js
@@ -0,0 +1,7 @@
+/*
+ criminal is constructing a ransom note. In order to disguise his handwriting, he is cutting out letters from a magazine.
+
+Given a magazine of letters and the note he wants to write, determine whether he can construct the word.
+*/
+console.log('canSpell', ['a', 'b', 'c', 'd', 'e', 'f'], 'bed'); //True
+console.log('canSpell', canSpell(['a', 'b', 'c', 'd', 'e', 'f'], 'cat'));//False
