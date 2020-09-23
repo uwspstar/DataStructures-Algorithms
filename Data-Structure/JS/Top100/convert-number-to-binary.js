d03e18c26bbb0c3b32fc6aa4b37711d8a06e16a9
xing wang <uwspstar@gmail.com>
2020-09-23 10:27:09 -0500

    daily

---
+++ Data-Structure/JS/Top100/convert-number-to-binary.js
@@ -0,0 +1,47 @@
+// use stack concept 10 -> binary (8-4-2-1)
+/* 10 -> 1010  
+10 / 2 = 5 ,  0
+5 / 2 = 2  ,  1
+2 / 2 = 1  ,  0
+1 / 2 = 0  ,  1
+
+0b1 0 1 0 =  8 * 1 + 0 * 4 + 1 * 2 + 0 * 1 = 10
+  8 4 2 1
+*/
+class Stack {
+    #result; // private 
+    constructor() {
+        this.#result = [];
+    }
+    push(data) {
+        return this.#result.push(data);
+    }
+    pop() {
+        return this.#result.pop()
+    }
+
+    isEmpty() {
+        return this.#result.length === 0;
+    }
+
+    getItem() {
+        return this.#result;
+    }
+}
+// revers int also can use stack
+const coverToBinary = function (num) {
+    let str = '';
+    let stack = new Stack(); // create a new Stack
+    while (num > 0) {
+        //3 -> 1, 
+        let mod = num % 2;
+        stack.push(mod)
+        num = num >> 1;
+    }
+    while (!stack.isEmpty()) {
+        str += stack.pop()
+    }
+    return str;
+}
+
+console.log('coverToBinary', coverToBinary(10));
