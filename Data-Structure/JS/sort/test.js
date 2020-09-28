acd028a844554c23c5319a5e34fb19ccb6a4c6d0
xing wang <uwspstar@gmail.com>
2020-09-28 06:04:39 -0500

    daily sort

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -60,14 +60,23 @@ const mergeSortHelp1 = (arr) => {
 }
 const mergeSortHelp = (arr) => {
     if (arr.length < 2) return arr;
-    const merge = (left, right) => { 
+    const merge = (left, right) => {
         if (left.length === 0) return right;
         if (right.length === 0) return left;
+        let result = [];
         while (left.length && right.length) {
-            
+            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
         }
-
+        return result.concat(left, right);
     }
+    const mergeSort = (arr) => {
+        if (arr.length < 2) return arr;
+        let mid = arr.length >> 1;
+        let left = mergeSort(arr.slice(0, mid));
+        let right = mergeSort(arr.slice(mid));
+        return merge(left, right);
+    }
+    return mergeSort(arr);
 }
 const insertionSort = (arr) => {
     if (arr.length < 2) return arr;
