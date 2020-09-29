40a7c85823b7be71cb26de56f5884c2b36e48bda
xing wang <uwspstar@gmail.com>
2020-09-29 06:37:03 -0500

    daily

--- Data-Structure/JS/Top100/00008-arr-subset.js
+++ Data-Structure/JS/Top100/00008-arr-subset.js
@@ -1,10 +1,18 @@
+/*
+The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+shallow copy : copy address
+*/
+
 const arrSubset = (arr) => {
     if (arr.length === 1) return [[], arr]
     let result = [[]];
     for (let value of arr) {
         const len = result.length;
         for (let i = 0; i < len; i++) {
+            console.log('result[i] =', JSON.stringify(result[i]));
             let temp = result[i].slice(0);
+
             console.log('value =', value, ', i =', i, ', temp =', result[i].slice(0), ', len =', len, ', result[i] =', result[i]);
 
             temp.push(value);
@@ -22,26 +30,26 @@ value = 1 , i = 0 , temp = [] , len = 1 , result[i] = []
 temp = [1]
 result = [[],[1]]
 ------------------
-value = 2 , i = 0 , temp = [] , len = 2 , result[i] = []
+value = 2 , i = 0 , temp = [] , len = 2 , result[0] = []
 temp = [2]
 result = [[],[1],[2]]
 ------------------
-value = 2 , i = 1 , temp = [ 1 ] , len = 2 , result[i] = [ 1 ]
+value = 2 , i = 1 , temp = [ 1 ] , len = 2 , result[1] = [ 1 ]
 temp = [1,2]
 result = [[],[1],[2],[1,2]]
 ------------------
-value = 3 , i = 0 , temp = [] , len = 4 , result[i] = []
+value = 3 , i = 0 , temp = [] , len = 4 , result[0] = []
 temp = [3]
 result = [[],[1],[2],[1,2],[3]]
 ------------------
-value = 3 , i = 1 , temp = [ 1 ] , len = 4 , result[i] = [ 1 ]
+value = 3 , i = 1 , temp = [ 1 ] , len = 4 , result[1] = [ 1 ]
 temp = [1,3]
 result = [[],[1],[2],[1,2],[3],[1,3]]
 ------------------
 value = 3 , i = 2 , temp = [ 2 ] , len = 4 , result[i] = [ 2 ]
 temp = [2,3]
 result = [[],[1],[2],[1,2],[3],[1,3],[2,3]]
-------------------
+------------------<result = [[],[1],[2],[1,2]]> temp = result[i].slice(0)
 value = 3 , i = 3 , temp = [ 1, 2 ] , len = 4 , result[i] = [ 1, 2 ]
 temp = [1,2,3]
 result = [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
