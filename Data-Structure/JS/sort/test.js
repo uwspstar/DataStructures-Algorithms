ac18df8778fb91d5ace93d776c9fa485565bea7f
xing wang <uwspstar@gmail.com>
2020-09-29 21:56:26 -0500

    daily sort

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -5,7 +5,7 @@
 // quickSort : pivot, swap, each step swap current index and pi, sort : low < hight
 
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
-const quickSortHelp = (arr) => {
+const quickSortHelp1 = (arr) => {
 
     if (arr.length < 2) return arr;
     // getPivotIndex with Swap
@@ -34,7 +34,33 @@ const quickSortHelp = (arr) => {
     }
     return quickSort(arr);
 }
-const mergeSortHelp1 = (arr) => {
+const quickSortHelp = (arr) => {
+    if (arr.length < 2) return arr;
+    let getPivotIndex = (arr, low = 0, high = arr.length - 1) => {
+        let pi = low;
+        let pivot = arr[high];
+        for (let i = low; i <= high; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, pi)
+                pi++;
+            }
+        }
+        swap(arr, pi, high);
+        return pi;
+    }
+    const quickSor = (arr, low = 0, high = arr.length - 1) => {
+        if (arr.length < 2) return arr;
+        if (low < high) {
+            let pi = getPivotIndex(arr, low, high);
+            quickSor(arr, low, pi - 1);
+            quickSor(arr, pi + 1, high);
+        }
+        return arr;
+    }
+
+
+}
+const mergeSortHelp = (arr) => {
     if (arr.length < 2) return arr;
 
     const merge = (left, right) => {
@@ -57,25 +83,6 @@ const mergeSortHelp1 = (arr) => {
     }
 
     return mergeSort(arr);
-}
-const mergeSortHelp = (arr) => {
-    if (arr.length < 2) return arr;
-    const merge = (left, right) => {
-        if (left.length === 0) return right;
-        if (right.length === 0) return left;
-        let result = [];
-        while (left && right) {
-            left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
-        }
-        return result.concat(left, right);
-    }
-    const mergeSort = (arr) => {
-        if (arr.length < 2) return arr;
-        let mid = arr.length >> 1;
-        let left = mergeSort(arr.slice(0, mid));
-        let right = mergeSort(arr.slice(mid));
-        return merge(left, right);
-    }
 }
 const insertionSort = (arr) => {
     if (arr.length < 2) return arr;
