9c2478a2ab4a3856b0d460868cde8dda8bfed5cc
xing wang <uwspstar@gmail.com>
2020-10-02 11:30:55 -0500

    daily

--- Data-Structure/JS/Top100/00001-fibonacci-recursive.js
+++ Data-Structure/JS/Top100/00001-fibonacci-recursive.js
@@ -105,3 +105,34 @@ console.log('fibonacciMemo', fibonacciMemo(45))// 1134903170
 // b = a - b
 // a = a - b
 
+// https://wangdoc.com/es6/function.html#%E5%B0%BE%E9%80%92%E5%BD%92
+
+//非尾递归的 Fibonacci 数列实现如下。
+
+function Fibonacci(n) {
+    if (n <= 1) { return 1 };
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+Fibonacci(10) // 89
+Fibonacci(100) // 超时
+Fibonacci(500) // 超时
+
+//尾递归优化过的 Fibonacci 数列实现如下。
+
+function Fibonacci2(n, ac1 = 1, ac2 = 1) {
+    if (n <= 1) { return ac2 };
+
+    return Fibonacci2(n - 1, ac2, ac1 + ac2);
+}
+
+/*
+
+Fibonacci2(100) // 573147844013817200000
+Fibonacci2(1000) // 7.0330367711422765e+208
+Fibonacci2(10000) // Infinity
+
+由此可见，“尾调用优化”对递归操作意义重大
+
+*/
