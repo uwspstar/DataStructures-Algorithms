b6ca0ef6a7d0354aa8287c93e723ef8700c55347
xing wang <uwspstar@gmail.com>
2020-10-04 07:02:53 -0500

    daily

--- Data-Structure/JS/Weekend/test20201003.js
+++ Data-Structure/JS/Weekend/test20201003.js
@@ -82,3 +82,32 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
     console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
+//quickSort
+{
+    const quickSortHelp = (arr) => {
+        if (arr.length < 2) return arr;
+        const getPivotIndex = (arr, low = 0, high = arr.length - 1) => {
+            let pi = low;
+            let pivot = arr[high];
+            for (let i = low; i <= high; i++) {
+                if (arr[i] < pivot) {
+                    swap(arr, i, pi);
+                    pi++;
+                }
+            }
+            return pi;
+        }
+        const quickSort = (arr, low = 0, high = arr.length - 1) => {
+            if (arr.length < 2) return arr;
+            if (low < high) {
+                let pi = getPivotIndex(arr, low, high);
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+
+            }
+            return arr;
+        }
+        return quickSort(arr);
+    }
+    console.log('5: quickSortHelp', JSON.stringify(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
+}
