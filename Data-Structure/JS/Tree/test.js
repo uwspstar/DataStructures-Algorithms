9f83471fae056a2fca8fe8b0d78be6e1a02e974c
xing wang <uwspstar@gmail.com>
2020-10-08 19:55:44 -0500

    tree

--- Data-Structure/JS/Tree/test.js
+++ Data-Structure/JS/Tree/test.js
@@ -6,10 +6,44 @@ class Node {
     }
 }
 class Tree {
+    constructor() {
+        this.root = null;
+    }
+    //8,3,2,9
+    insertNode(node, newNode) {
 
+        if (node.value > newNode.value) { //left
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else if (node.value < newNode.value) {//right
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        return this.root === null
+            ? this.root = newNode
+            : this.insertNode(this.root, newNode)
+    }
+    getRoot() {
+        return this.root;
+    }
 }
 
+let t = new Tree();
+t.insert(8);
+t.insert(3);
+t.insert(2);
+t.insert(9);
 
+console.log("t-root", JSON.stringify(t));
 /*
 var Tree = function () {
     var node = function (value) {
