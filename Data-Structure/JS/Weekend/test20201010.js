e448c4e7de81af19f28423adc70ae192b7318e59
xing wang <uwspstar@gmail.com>
2020-10-10 07:54:42 -0500

    weekend

--- Data-Structure/JS/Weekend/test20201010.js
+++ Data-Structure/JS/Weekend/test20201010.js
@@ -55,18 +55,62 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 }
 //mergeSort
 {
-    
+    const mergeSortHelp = (arr) => {
+        if (arr.length < 2) return arr;
+        const merge = (left, right) => {
+            if (left.length === 0) return right;
+            if (right.length === 0) return left;
+            let result = [];
+            while (left.length && right.length) {
+                left[0] < right[0]
+                    ? result.push(left.shift())
+                    : result.push(right.shift());
+            }
+            return result.concat(left, right);
+        }
+        const mergeSort = (arr) => {
+            if (arr.length < 2) return arr;
+            let mid = arr.length >> 1;
+            let left = mergeSort(arr.slice(0, mid));
+            let right = mergeSort(arr.slice(mid));
+            return merge(left, right);
+        }
+        return mergeSort(arr);
+    }
     console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
-
 }
 //quickSort
 {
-
+    const quickSortHelp = (arr) => {
+        if (arr.length < 2) return arr;
+        let getPivotIndex = (arr, low = 0, high = arr.length - 1) => {
+            let pi = low;
+            let pivot = arr[high];
+            for (let i = low; i <= high; i++) {
+                if (arr[i] < pivot) {
+                    swap(arr, i, pi);
+                    pi++;
+                }
+            }
+            swap(arr, pi, high);
+            return pi;
+        }
+        const quickSort = (arr, low = 0, high = arr.length - 1) => {
+            if (arr.length < 2) return arr;
+            if (low < high) {
+                let pi = getPivotIndex(arr, low, high);
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+            return arr;
+        }
+        return quickSort(arr);
+    }
     console.log('5: quickSortHelp', JSON.stringify(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 // reverseInteger
 {
-
+    
     console.log('1: reverseInteger', JSON.stringify(reverseInteger(-321))); // -123
 }
 // buySellStock
