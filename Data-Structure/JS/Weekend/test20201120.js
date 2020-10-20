676c980b8948ac37abe94b1e46ed82c5e53b273b
xing wang <uwspstar@gmail.com>
2020-10-20 10:13:11 -0500

    daily sort

--- Data-Structure/JS/Weekend/test20201120.js
+++ Data-Structure/JS/Weekend/test20201120.js
@@ -2,22 +2,83 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 console.log('================ start sort ================');
 //bubbleSort
 {
-    //console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const bubbleSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let isSwap = false;
+            for (let j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1)
+                    isSwap = true;
+                }
+            }
+            if (!isSwap) break;
+        }
+        return arr;
+    }
+    console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //selectionSort
 {
-
-    //console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const selectionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 0; i < arr.length; i++) {
+            let smallIndex = i;
+            for (let j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallIndex])
+                    smallIndex = j;
+            }
+            if (smallIndex !== i) swap(arr, smallIndex, i);
+        }
+        return arr;
+    }
+    console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //insertionSort
 {
-
-    //console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const insertionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 1; i < arr.length; i++) {
+            let current = arr[i];
+            let j = i - 1;
+            for (; j >= 0; j--) {
+                if (arr[j] > current) {
+                    arr[j + 1] = arr[j];
+                } else break;
+            }
+            arr[j + 1] = current;
+        }
+        return arr;
+    }
+    console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
 //mergeSort
 {
+    const mergeSortHelp = arr => {
+        if (arr.length < 2) return arr;
+        const merge = (left, right) => {
+            if (left.length === 0) return right;
+            if (right.length === 0) return left;
+            let result = [];
+            while (left.length && right.length) {
+                left[0] < right[0]
+                    ? result.push(left.shift())
+                    : result.push(right.shift());
+            }
+            return result.concat(left, right);
+        }
 
+        const mergeSort = arr => {
+            if (arr.length < 2) return arr;
+            let mid = arr.length >> 1;
+            let left = mergeSort(arr.slice(0, mid));
+            let right = mergeSort(arr.slice(mid));
+            return merge(left, right);
+        }
+
+        return mergeSort(arr);
+    }
     //console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
