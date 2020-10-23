7f26f9f9976dd716e10d78bcc64f186042e52c81
xing wang <uwspstar@gmail.com>
2020-10-23 16:19:17 -0500

    daily

--- Data-Structure/JS/Weekend/test20201122.js
+++ Data-Structure/JS/Weekend/test20201122.js
@@ -29,7 +29,7 @@ console.log('================ start sort ================');
                     smallIndex = j;
                 }
             }
-            if ( smallIndex !== i) swap(arr, smallIndex, i);
+            if (smallIndex !== i) swap(arr, smallIndex, i);
         }
         return arr;
     }
@@ -87,13 +87,29 @@ console.log('================ start sort ================');
     const quickSortHelp = arr => {
         if (arr.length < 2) return arr;
         const getPivotIndex = (arr, low = 0, high = arr.length - 1) => {
-
+            let pi = low;
+            let pivot = arr[high];
+            for (let i = low; i <= high; i++) {
+                if (arr[i] < pivot) {
+                    swap(arr, i, pi);
+                    pi++;
+                }
+            }
+            swap(arr, pi, high);
+            return pi;
         }
         const quickSort = (arr, low = 0, high = arr.length - 1) => {
-            
+            if (arr.length < 2) return arr;
+            if (low < high) {
+                let pi = getPivotIndex(arr, low, high);
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+            return arr;
         }
+        return quickSort(arr);
     }
-    //console.log('5: quickSortHelp', JSON.stringify(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
+    console.log('5: quickSortHelp', JSON.stringify(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 console.log('================ end sort ================');
 
