fd1dff6ee320c7d6dbbdd29afb6f21b40435680d
xing wang <uwspstar@gmail.com>
2020-11-07 18:24:31 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201108.js
+++ Data-Structure/JS/Weekend/test20201108.js
@@ -20,12 +20,26 @@ console.log('================ start sort ================');
 }
 //selectionSort
 {
-
-    //console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const selectionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 0; i < arr.length; i++) {
+            let smallIndex = i;
+            for (let j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallIndex]) {
+                    smallIndex = j;
+                }
+            }
+            if (smallIndex !== i) swap(arr, smallIndex, i);
+        }
+        return arr;
+    }
+    console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //insertionSort
 {
-
+    const insertionSort = arr => {
+        
+    }
     //console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
