fdbbc6120783f004c2c42d00ced31eaaad0a629e
xing wang <uwspstar@gmail.com>
2020-12-02 19:25:22 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201130.js
+++ Data-Structure/JS/Weekend/test20201130.js
@@ -375,12 +375,37 @@ console.log('================ start tree ================');
         constructor() {
             this.root = null;
         }
+        //8-3-2-9
         insert(value) {
-
+            let newNode = new TreeNode(value);
+            this.root === null
+                ? this.root = newNode
+                : this.insertNewNode(this.root, newNode);
+            return this;
+        }
+        insertNewNode(node, newNode) {
+            if (node.value > newNode.value) {
+                node.left === null
+                    ? node.left = newNode
+                    : this.insertNewNode(node.left, newNode);
+            } else {
+                node.right === null
+                    ? node.right = newNode
+                    : this.insertNewNode(node.right, newNode);
+            }
         }
         traversal() { }
         search(value) { }
+        getRoot() {
+            return this.root;
+        }
     }
 
+    let t = new Tree();
+    t.insert(8);
+    t.insert(2);
+    t.insert(3);
+    t.insert(9);
+    console.log('t.getRoot()', JSON.stringify(t.getRoot()));
+    console.log('================ end tree ================');
 }
-console.log('================ end tree ================');
