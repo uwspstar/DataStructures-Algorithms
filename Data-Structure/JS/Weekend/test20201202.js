c3ca531d11fb6bf2369c63cc797148ae5758fee5
xing wang <uwspstar@gmail.com>
2020-12-03 18:24:59 -0600

    daily sort

---
+++ Data-Structure/JS/Weekend/test20201202.js
@@ -0,0 +1,156 @@
+// Whole-script strict mode syntax
+"use strict";
+
+const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+console.log('================ start sort ================');
+//bubbleSort
+{
+    //console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
+}
+//selectionSort
+{
+
+    //console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+}
+//insertionSort
+{
+
+    //console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+
+}
+//mergeSort
+{
+
+    //console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
+
+}
+//quickSort
+{
+
+    //console.log('5: quickSortHelp', JSON.stringify(quickSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
+}
+console.log('================ end sort ================');
+
+console.log('================ start top 100 ================');
+// reverseInteger
+{
+
+    //console.log('1: reverseInteger', JSON.stringify(reverseInteger(-321))); // -123
+}
+// buySellStock
+{
+
+    //console.log('2: buySellStock', JSON.stringify(buySellStock([9, 11, 8, 5, 7, 10]))); // 5
+}
+// fibEndCall - tail call
+{
+    //console.log('3: fibEndCall', JSON.stringify(fibEndCall(45))); //1, 1, 2, 3, 5,..., 1134903170
+}
+// fibonacci - memo
+{
+    //console.log('4: fibonacci', JSON.stringify(fibonacci(45))); //1, 1, 2, 3, 5,..., 1134903170
+}
+// isBalance
+{
+
+    //console.log('5: isBalance: ', JSON.stringify(isBalance("[{()()}]")));//true
+    //console.log('5: isBalance: ', JSON.stringify(isBalance("[[[]")));//false
+}
+// isPalindrome
+{
+    //console.log('6: isPalindrome', JSON.stringify(isPalindrome('amanaplanacanalpanama'))) // true
+}
+// sortColor
+{
+    //console.log('7: sortColor', JSON.stringify(sortColor([0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1])));
+}
+// matrixSpiral
+{
+    const arr = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20]
+    ]
+
+    //console.log('8: matrixSpiral', JSON.stringify(matrixSpiral(arr)));
+    // [1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12]
+}
+// maxWaterContainer
+{
+    // area = height x width
+    // height = min (a, b)
+    // width = bi - ai
+
+    //console.log('9-1: maxArea = ', maxWaterContainer([7])) //0
+    //console.log('9-2: maxArea = ', maxWaterContainer([7, 1])) // 1
+    //console.log('9-3: maxWaterContainer', JSON.stringify(maxWaterContainer([7, 1, 2, 3, 9]))); //28
+
+}
+// longestSubString
+{
+    //console.log('10-1: longestSubString', JSON.stringify(longestSubString('asdfadsfasavcbdferes'))); // 9
+    //console.log('10-2: longestSubString', JSON.stringify(longestSubString('asdfads'))); // 4
+}
+console.log('================ end top 100 ================');
+
+console.log('================ start linkedList ================');
+// LinkList
+{
+    class Node {
+        constructor(value) {
+            this.value = value;
+            this.next = null
+        }
+    }
+    class LinkedList {
+        constructor(value) {
+            this.head = new Node(value);
+            // cannot use if (this.head === null) this.head = new Node(value);
+            // the new create a empty obj with constructor
+        }
+        append(value) { }
+        reverse() { }
+        findMid() { }
+        findLastKth(k) { }
+        isCircularFastSlow() { }
+
+    }
+    let linkedList = new LinkedList(5);
+    linkedList.append(10);
+    linkedList.append(20);
+    linkedList.append(30);
+    linkedList.append(40);
+    linkedList.append(50);
+    console.log('linkedList = ', JSON.stringify(linkedList));
+    console.log('findLastKth(3) = ', JSON.stringify(linkedList.findLastKth(3)));
+    console.log('findMid = ', JSON.stringify(linkedList.findMid()));
+    console.log('reverse = ', JSON.stringify(linkedList.reverse()));
+    console.log(JSON.stringify(linkedList.isCircularFastSlow()));
+    console.log('linkedList = ', JSON.stringify(linkedList));
+}
+console.log('================ end linkedList ================');
+
+console.log('================ start tree ================');
+// Tree
+{
+    class TreeNode {
+        constructor(value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    class Tree {
+        constructor() {
+            this.root = null;
+        }
+        insert(value) {
+
+        }
+        traversal() { }
+        search(value) { }
+    }
+
+}
+console.log('================ end tree ================');
