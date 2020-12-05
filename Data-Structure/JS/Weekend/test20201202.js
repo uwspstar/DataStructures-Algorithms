ead8cae0e8dd86be0fc2afe7da2455fcba32f37d
xing wang <uwspstar@gmail.com>
2020-12-04 18:54:59 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201202.js
+++ Data-Structure/JS/Weekend/test20201202.js
@@ -5,7 +5,21 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 console.log('================ start sort ================');
 //bubbleSort
 {
-    //console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const bubbleSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let isSwap = false;
+            for (let j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    isSwap = true;
+                }
+                if (!isSwap) break;
+            }
+            return arr;
+        }
+    }
+    console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //selectionSort
 {
