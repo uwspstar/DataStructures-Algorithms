c050c9666eaa9605a0b11f41b68efc93b0acb033
xing wang <uwspstar@gmail.com>
2020-12-06 15:10:36 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201202.js
+++ Data-Structure/JS/Weekend/test20201202.js
@@ -23,13 +23,39 @@ console.log('================ start sort ================');
 }
 //selectionSort
 {
-
-    //console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const selectionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 0; i < arr.length; i++) {
+            let smallIndex = i;
+            for (let j = i; j < arr.length; j++) {
+                if (arr[j] < arr[smallIndex]) {
+                    smallIndex = j;
+                }
+            }
+            if (smallIndex !== i) swap(arr, smallIndex, i);
+        }
+        return arr;
+    }
+    console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //insertionSort
 {
+    const insertionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 1; i < arr.length; i++) {
+            let current = arr[i];
+            let j = i - 1;
+            for (; j >= 0; j--) {
+                if (arr[j] > current) {
+                    arr[j + 1] = arr[j];
+                } else break;
+            }
+            arr[j + 1] = current;
+        }
+        return arr;
+    }
 
-    //console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    console.log('3: insertionSort', JSON.stringify(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
 //mergeSort
