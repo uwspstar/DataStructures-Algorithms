11d69fb5b5757459b2ba3123c46bef040a61a402
xing wang <uwspstar@gmail.com>
2020-12-25 17:51:08 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201221.js
+++ Data-Structure/JS/Weekend/test20201221.js
@@ -210,9 +210,22 @@ console.log('================ start top 100 ================');
 {
     const sortColor = arr => {
         if (arr.length < 2) return arr;
-
+        let p0 = 0;
+        let current = 0;
+        let p2 = arr.length - 1;
+        while (current <= p2) {
+            if (arr[current] === 2) {
+                swap(arr, current, p2);
+                p2--;
+            } else if (arr[current] === 0) {
+                swap(arr, current, p0);
+                p0++;
+                current++;
+            } else current++;
+        }
+        return arr;
     }
-    //console.log('7: sortColor', JSON.stringify(sortColor([0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1])));
+    console.log('7: sortColor', JSON.stringify(sortColor([0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1])));
 }
 // matrixSpiral
 {
