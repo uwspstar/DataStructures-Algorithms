4cc8fef2ce31aaccb262d16fdec9ebf21b3ee133
xing wang <uwspstar@gmail.com>
2020-12-26 09:31:11 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20201221.js
+++ Data-Structure/JS/Weekend/test20201221.js
@@ -235,7 +235,20 @@ console.log('================ start top 100 ================');
         [11, 12, 13, 14, 15],
         [16, 17, 18, 19, 20]
     ]
-
+    const matrixSpiral = arr => {
+        if (arr.length < 2) return arr[0];
+        let rowStart = 0
+        let rowEnd = arr.length - 1;
+        let colStart = 0;
+        let colEnd = arr[0].length - 1;
+        let result = [];
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (let i = colStart; i < colEnd; i++) {
+                result.push(arr[rowStart][i]);
+            } 
+            rowStart++;
+        }
+    }
     //console.log('8: matrixSpiral', JSON.stringify(matrixSpiral(arr)));
     // [1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12]
 }
