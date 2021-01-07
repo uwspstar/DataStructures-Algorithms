f8e5194b9ad363f4885906c53d8e90fc1f005880
xing wang <uwspstar@gmail.com>
2021-01-07 10:29:47 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20210105.js
+++ Data-Structure/JS/Weekend/test20210105.js
@@ -59,8 +59,21 @@ console.log('================ start sort ================');
 }
 //mergeSort
 {
-
-    //console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
+    const mergeSortHelp = arr => {
+        if (arr.length < 2) return arr;
+        const merge = (left, right) => {
+            if (left.length === 0) return right;
+            if (right.length === 0) return left;
+            let result = []
+            while (left.length && right.length) {
+                left[0] < right[0]
+                    ? result.push(left.shift())
+                    : result.push(right.shift());
+            }
+            return result.concat(left, right)
+        }
+    }
+    console.log('4: mergeSortHelp', JSON.stringify(mergeSortHelp([8, 1, 2, 3, 4, 5, 6, 7])));
 
 }
 //quickSort
