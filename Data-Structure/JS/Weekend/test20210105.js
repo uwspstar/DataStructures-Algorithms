fde10726a312be1c2f88f04734621dd873da87ad
xing wang <uwspstar@gmail.com>
2021-01-07 10:19:54 -0600

    daily sort

--- Data-Structure/JS/Weekend/test20210105.js
+++ Data-Structure/JS/Weekend/test20210105.js
@@ -5,12 +5,38 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
 console.log('================ start sort ================');
 //bubbleSort
 {
-    //console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const bubbleSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let isSwap = false;
+            for (let j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    isSwap = true;
+                }
+            }
+            if (!isSwap) break;
+        }
+        return arr;
+    }
+    console.log('1: bubbleSort   ', JSON.stringify(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //selectionSort
 {
-
-    //console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
+    const selectionSort = arr => {
+        if (arr.length < 2) return arr;
+        for (let i = 0; i < arr.length; i++) {
+            let smallIndex = i;
+            for (let j = smallIndex; j < arr.length; j++) {
+                if (arr[j] < arr[smallIndex]) {
+                    smallIndex = j;
+                }
+            }
+            if (smallIndex !== i) swap(arr, smallIndex, i);
+        }
+        return arr;
+    }
+    console.log('2: selectionSort', JSON.stringify(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])));
 }
 //insertionSort
 {
