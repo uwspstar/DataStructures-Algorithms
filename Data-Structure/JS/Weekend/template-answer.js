34b58a4234d01cb9b21a109f72a867d57d04ae8a
xing wang <uwspstar@gmail.com>
2021-03-13 18:08:16 -0600

    daily

--- Data-Structure/JS/Weekend/template-answer.js
+++ Data-Structure/JS/Weekend/template-answer.js
@@ -3,6 +3,74 @@
 
 //swap function 
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+// Ransom Note : Array
+// https://leetcode.com/problems/ransom-note/
+{
+    const canConstruct = (ransomNote, magazine) => {
+        //你可以假设两个字符串均只含有小写字母
+        if (magazine.length < ransomNote.length) return false;
+        let arr = Array(26).fill(0);
+        console.log('arr1 :', arr);
+        for (let i = 0; i < magazine.length; i++) {
+            let code = magazine.charCodeAt(i);
+            console.log('i', i, 'code1 :', code);
+            arr[code - 97]++;
+        }
+        console.log('arr2 :', arr);
+        for (let i = 0; i < ransomNote.length; i++) {
+            let code = ransomNote.charCodeAt(i);
+            console.log('i', i, 'code1 :', code);
+            arr[code - 97]--;
+            console.log('code : ', code, '-- arr[code - 97]', arr[code - 97]);
+            if (arr[code - 97] < 0) return false;
+            console.log('arr4 :', arr);
+        }
+        console.log('arr3 :', arr);
+        return true;
+    }
+    console.log('canConstruct', canConstruct('aa', 'ab'));
+}
+{
+    var canConstruct = function (ransomNote, magazine) {
+        let magazineCopy = magazine
+        if (ransomNote.length > magazine.length) return false;
+        let ransomArr = ransomNote.split("")
+        ransomArr.forEach(function replacer(item, index) {
+            magazine = magazine.replace(item, "");
+        });
+        return magazine.length === magazineCopy.length - ransomArr.length;
+    };
+}
+//fourSum : Map
+// https://leetcode.com/problems/4sum-ii/
+// O(N^2)
+{
+    const fourSum = (arr1, arr2, arr3, arr4) => {
+        let map1 = new Map();
+        let count = 0;
+        for (let i = 0; i < arr1.length; i++) {
+            for (let j = 0; j < arr2.length; j++) {
+                let key = arr1[i] + arr2[j];
+                let value = map1.get(key);
+                value ? map1.set(key, value + 1) : map1.set(key, 1);
+            }
+        }
+        for (let i = 0; i < arr3.length; i++) {
+            for (let j = 0; j < arr4.length; j++) {
+                let key = arr3[i] + arr4[j];
+                let value = map1.get(-1 * key);
+                value ? count += value : {}
+            }
+        }
+        return count;
+    }
+
+    let A = [1, 2]
+    let B = [-2, -1]
+    let C = [-1, 2]
+    let D = [0, 2]
+    console.log('fourSum', fourSum(A, B, C, D))
+}
 // sliding window : minSubArrayLen  
 // 209 https://leetcode-cn.com/problems/minimum-size-subarray-sum/
 // minSubArrayLen
@@ -180,11 +248,13 @@ const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
         constructor() { };
         arrUnion(arr1, arr2) {
             return [...new Set([...arr1, ...arr2])];
+
         }
         arrAnd(arr1, arr2) {
             let a = [...new Set(arr1)];
             let b = new Set(arr2);
             return arr1.filter(x => b.has(x));
+            //return [...new Set(nums1)].filter(n => nums2.includes(n))
         }
         arrDifference(arr1, arr2) {
             let a = [...new Set(arr1)];
--- Data-Structure/JS/Weekend/test.js
+++ Data-Structure/JS/Weekend/test.js
@@ -1,7 +1,36 @@
 {
-    console.log('ab'.charCodeAt(0) - 'ab'.charCodeAt(0));
-    console.log('ab'.charCodeAt(1) - 'ab'.charCodeAt(0));
-    let result = Array(26).fill(0);
-    console.log('result =', result);
+    const canConstruct =  (ransomNote, magazine) =>{
+        //你可以假设两个字符串均只含有小写字母
+        if (magazine.length < ransomNote.length) return false;
+        let arr = Array(26).fill(0);
+        console.log('arr1 :', arr);
+        for (let i = 0; i < magazine.length; i++) {
+            let code = magazine.charCodeAt(i);
+            console.log('i', i, 'code1 :', code);
+            arr[code - 97]++;
+        }
+        console.log('arr2 :', arr);
+        for (let i = 0; i < ransomNote.length; i++) {
+            let code = ransomNote.charCodeAt(i);
+            console.log('i', i, 'code1 :', code);
+            arr[code - 97]--;
+            console.log('code : ', code, '-- arr[code - 97]', arr[code - 97]);
+            if (arr[code - 97] < 0) return false;
+            console.log('arr4 :', arr);
+        }
+        console.log('arr3 :', arr);
+        return true;
+    }
+    console.log('canConstruct', canConstruct('aa', 'ab'));
 }
-
+{
+    var canConstruct = function(ransomNote, magazine) {
+        let magazineCopy = magazine
+        if (ransomNote.length > magazine.length) return false;
+        let ransomArr = ransomNote.split("")
+        ransomArr.forEach(function replacer(item, index) {
+          magazine = magazine.replace(item, "");
+        });
+        return magazine.length === magazineCopy.length - ransomArr.length;
+      };
+}
