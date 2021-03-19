066ebfdf8ecb3467bf0e9a7f33c1fe98cf272535
xing wang <uwspstar@gmail.com>
2021-03-19 08:23:02 -0500

    daily

--- Data-Structure/JS/Weekend/template-answer.js
+++ Data-Structure/JS/Weekend/template-answer.js
@@ -3,6 +3,46 @@
 
 //swap function 
 const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+// reverseStr jump kth
+{
+    var reverseStr = function (s, k) {
+        let n = 0, result = '';
+        for (let i = 0; i < s.length; i += k) {
+            let t = s.slice(i, i + k);
+            n++;
+            if (n % 2 === 1) {
+                t = t.split('').reverse().join('');
+            }
+            result += t;
+        }
+        return result;
+    };
+}
+// reverseStr jump kth
+{
+    var reverseStr = function (s, k) {
+        if (s.length < 2) return s;
+        //const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+        const reverse = s => {
+            if (s.length < 2) return s;
+            return s.split('').reverse().join('');
+        }
+
+        let len = s.length;
+        if (len <= k) return reverse(s);
+
+        let result = '';
+        while (len > 0) {
+            result += reverse(s.slice(0, k));
+            result += s.slice(k, 2 * k);
+            s = s.slice(2 * k);
+            len = s.length;
+        }
+        return result;
+    };
+
+    console.log('abcdefg', reverseStr('abcdefg', 2));
+}
 //
 // Power Set : all sub set
 {
--- Data-Structure/JS/Weekend/test.js
+++ Data-Structure/JS/Weekend/test.js
@@ -1,83 +1,39 @@
-let log = console.log;
+
 {
-    const Swap = (a = 1, b = 2) => {
-        log('a:', a, 'b', b);
-        if (a !== b) {
-            a ^= b;
-            b ^= a;
-            a ^= b;
+    var reverseStr = function (s, k) {
+        let n = 0, result = '';
+        for (let i = 0; i < s.length; i += k) {
+            let t = s.slice(i, i + k);
+            n++;
+            if (n % 2 === 1) {
+                t = t.split('').reverse().join('');
+            }
+            result += t;
         }
-        log('a:', a, 'b', b);
-    }
-
-    Swap(1, 2);
-}
-
-{
-    const set = ['x', 'y', 'z'];
-    function allSubsets(arr) {
-        if (arr.length === 0) { return [[]]; }
-        const prev = allSubsets(arr.slice(1));
-        console.log('prev', prev)
-        const next = prev.map(el => [...el, arr[0]]);
-        return [...prev, ...next];
-    }
-    console.log('1: allSubsets', allSubsets(set));
-}
-/*
-{
-    const set = ['x', 'y', 'z'];
-    function allSubsets(arr) {
-        const maxIndex = arr.length - 1;
-        let result = [[]];
-        arr.forEach(el => {
-            result.forEach(subset => {
-                result.push([...subset, el]);
-            })
-        })
         return result;
-    }
-    console.log('2: allSubsets', allSubsets(set));
-}
-// Power Set : all sub set
-{
-    const set = ['x', 'y', 'z'];
-    const powerSet = (arr = []) => {
-        const res = [];
-        const { length } = arr;
-        const numberOfCombinations = 2 ** length;
-        for (let combinationIndex = 0; combinationIndex < numberOfCombinations; combinationIndex += 1) {
-            const subSet = [];
-            for (let setElementIndex = 0; setElementIndex < arr.length;
-                setElementIndex += 1) {
-                if (combinationIndex & (1 << setElementIndex)) {
-                    subSet.push(arr[setElementIndex]);
-                };
-            };
-            res.push(subSet);
-        };
-        return res;
     };
-    console.log(powerSet(set));
 }
-// Power Set : all sub set
 {
-    const set = ['x', 'y', 'z'];
-    const powerSet = (arr = []) => {
-        const res = [];
-        const { length } = arr;
-        const len = 2 ** length;
-        for (let i = 0; i < len; i++) {
-            const subSet = [];
-            for (let j = 0; j < arr.length; j++) {
-                if (i & (1 << j)) {
-                    subSet.push(arr[j]);
-                };
-            };
-            res.push(subSet);
-        };
-        return res;
+    var reverseStr = function (s, k) {
+        if (s.length < 2) return s;
+        //const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+        const reverse = s => {
+            if (s.length < 2) return s;
+            return s.split('').reverse().join('');
+        }
+
+        let len = s.length;
+        if (len <= k) return reverse(s);
+
+        let result = '';
+        while (len > 0) {
+            result += reverse(s.slice(0, k));
+            result += s.slice(k, 2 * k);
+            s = s.slice(2 * k);
+            len = s.length;
+        }
+        return result;
     };
-    console.log(powerSet(set));
+
+    console.log('abcdefg', reverseStr('abcdefg', 2));
 }
-*/
