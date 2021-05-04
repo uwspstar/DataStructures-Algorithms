4b085b5a9fd3cf63b9abb9c3ad9d1ddeeaa27906
xing wang <uwspstar@gmail.com>
2021-05-04 14:20:34 -0500

    daily

---
+++ Data-Structure/Nodejs/Baisc/Tree/leetcode/145. Binary Tree Postorder Traversal.js
@@ -0,0 +1,56 @@
+//145. Binary Tree Postorder Traversal
+//Given the root of a binary tree, return the postorder traversal of its nodes' values.
+/*
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+*/
+
+{
+    const postorderTraversal = root => {
+        if (root === null) return [];
+
+        let stack = [root];
+        let outStack = [];
+        let res = [];
+
+        while (stack.length > 0) {
+            let n = stack.pop();
+            outStack.push(n);
+            if (n.left !== null) {
+                stack.push(n.left);
+            }
+            if (n.right !== null) {
+                stack.push(n.right);
+            }
+        }
+
+        while (outStack.length > 0) {
+            let n = outStack.pop();
+            res.push(n.val);
+        }
+
+        return res;
+    };
+}
+{
+    var postorderTraversal = function (root) {
+        if (root === null) return [];
+
+        let res = [];
+
+        const dfs_postOrder = n => {
+            if (n === null) return [];
+            n.left && dfs_postOrder(n.left);
+            n.right && dfs_postOrder(n.right);
+            res.push(n.val) //logic : self
+        }
+
+        dfs_postOrder(root)
+        return res;
+    };
+
+}
+
+// 316. Remove Duplicate Letters
+// 439. Ternary Expression Parser
+// 652. Find Duplicate Subtrees
---
+++ Data-Structure/Nodejs/Baisc/Tree/leetcode/255. Verify Preorder Sequence in Binary Search Tree.js
@@ -0,0 +1,15 @@
+//255. Verify Preorder Sequence in Binary Search Tree
+//Given an array of unique integers preorder, return true if it is the correct preorder traversal sequence of a binary search tree
+/*
+Input: preorder = [5,2,1,3,6]
+Output: true
+*/
+{
+    const verifyPreorder = preorder => {
+
+        //left i * 2 + 1
+        //right i * 2 + 2
+        
+    
+    };
+}
