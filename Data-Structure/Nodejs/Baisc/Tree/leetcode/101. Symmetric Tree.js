be468d53db7972bb9873820a29bbb10c121b617e
xing wang <uwspstar@gmail.com>
2021-05-05 14:49:44 -0500

    daily

--- Data-Structure/Nodejs/Baisc/Tree/leetcode/101. Symmetric Tree.js
+++ Data-Structure/Nodejs/Baisc/Tree/leetcode/101. Symmetric Tree.js
@@ -1,3 +1,21 @@
 //101. Symmetric Tree
 //Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 //Input: root = [1,2,2,3,4,4,3] Output: true
+{
+    const isMirror = (n1, n2) => {
+        if (n1 === null && n2 === null) return true;
+
+        if (n1 === null && n2 !== null) return false;
+        if (n1 !== null && n2 === null) return false;
+        
+        if (n1.val !== n2.val) return false;
+        
+        return isMirror(n1.left, n2.right) && isMirror(n2.left, n1.right) 
+        
+    }
+    
+    var isSymmetric = function(root) {
+        if (root === null) return true;
+        return isMirror(root, root)
+    };
+}
