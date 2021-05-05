4d49a981896f4444006b73edc8c5db0b449cf806
xing wang <uwspstar@gmail.com>
2021-05-05 15:05:54 -0500

    daily

--- Data-Structure/Nodejs/Baisc/Tree/leetcode/101. Symmetric Tree.js
+++ Data-Structure/Nodejs/Baisc/Tree/leetcode/101. Symmetric Tree.js
@@ -7,15 +7,38 @@
 
         if (n1 === null && n2 !== null) return false;
         if (n1 !== null && n2 === null) return false;
-        
+
         if (n1.val !== n2.val) return false;
-        
-        return isMirror(n1.left, n2.right) && isMirror(n2.left, n1.right) 
-        
+
+        return isMirror(n1.left, n2.right) && isMirror(n2.left, n1.right)
+
     }
-    
-    var isSymmetric = function(root) {
+
+    var isSymmetric = function (root) {
         if (root === null) return true;
         return isMirror(root, root)
     };
 }
+{
+    var isSymmetric = function (root) {
+        if (root == null) return true;
+        let q = [root, root];
+        while (q.length > 0) {
+
+            let n1 = q.shift();
+            let n2 = q.shift();
+
+            if (n1 === null && n2 == null) continue;
+            if (!n1 || !n2) return false;
+            if (n1.val !== n2.val) return false;
+            q.push(n1.left, n2.right); // smart : so shift() twice work
+            q.push(n1.right, n2.left);
+        }
+        return true;
+    }
+}
+/*
+The Maze II
+Construct Binary Search Tree from Preorder Traversal
+Frog Position After T Seconds
+*/
