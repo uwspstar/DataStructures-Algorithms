7a8ea68f6f1b2b43b588244fc9b57b80ab54d3d3
xing wang <uwspstar@gmail.com>
2021-06-08 16:27:16 -0500

    daily

--- Data-Structure/Nodejs/175 Questions by Difficult/Easy/Bit Manipulation/268. Missing Number.js
+++ Data-Structure/Nodejs/175 Questions by Difficult/Easy/Bit Manipulation/268. Missing Number.js
@@ -4,6 +4,9 @@ Given an array nums containing n distinct numbers in the range [0, n], return th
 
 Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?  
 
+// O(N) liner
+// O(1) constance space
+
 Input: nums = [3,0,1] Output: 2
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums. 
 
@@ -15,10 +18,12 @@ Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0
 
 Input: nums = [0] Output: 1
 Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+
 */
 {
     var missingNumber = function (nums) {
         let missing = nums.length;
+
         for (let i = 0; i < nums.length; i++) {
             missing ^= i ^ nums[i];
         }
@@ -28,6 +33,8 @@ Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1
     //Couples Holding Hands
 }
 {
+    //O(N) 
+    //O(1)
     var missingNumber = function (nums) {
         let n = nums.length + 1;
         let sum = n * (n - 1) / 2;
