9ae33138c9295fb7f91414dcb5aa197861f0f497
xing wang <uwspstar@gmail.com>
2021-06-11 17:34:51 -0500

    daily

---
+++ Data-Structure/Nodejs/LeetCode by Category/Math/118. Pascal's Triangle.js
@@ -0,0 +1,22 @@
+//118. Pascal's Triangle
+{
+    var generate = function (numRows) {
+        if (numRows === 1) return [[1]];
+        let res = [[1], [1, 1]];
+        for (let i = 2; i < numRows; i++) {
+            res[i] = [];
+            res[i].push(1);
+            let tmp = res[i - 1];
+            for (let j = 1; j < tmp.length; j++) {
+                let sum = tmp[j] + tmp[j - 1];
+                res[i].push(sum);
+            }
+            res[i].push(1);
+        }
+
+        return res;
+    };
+    //Remove Sub-Folders from the Filesystem
+    //Divide Array in Sets of K Consecutive Numbers
+    //Sort Array by Increasing Frequency
+}
