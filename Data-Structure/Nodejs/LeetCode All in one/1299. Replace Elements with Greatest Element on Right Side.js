a91cec86f91e18a715607e70c333520bfa4b1be2
xing wang <uwspstar@gmail.com>
2021-06-12 14:52:25 -0500

    daily

---
+++ Data-Structure/Nodejs/LeetCode All in one/1299. Replace Elements with Greatest Element on Right Side.js
@@ -0,0 +1,77 @@
+// backward , and 
+
+//1299. Replace Elements with Greatest Element on Right Side
+/*
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Input: arr = [17,18,5,4,6,1] Output: [18,6,6,6,1,-1]
+Explanation: 
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+
+Example 2:
+
+Input: arr = [400] Output: [-1]
+Explanation: There are no elements to the right of index 0.
+*/
+
+{
+    //backward O(N)
+    var replaceElements = function (arr) {
+        if (arr.length < 2) return [-1];
+        let max = -1;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            max = Math.max(arr[i], arr[i] = max);
+            /*
+            let tmp = max;
+            max = Math.max(arr[i], max);
+            arr[i] = tmp;
+            */
+        }
+        return arr;
+    };
+    //High Five
+    //Special Positions in a Binary Matrix
+    //Get Biggest Three Rhombus Sums in a Grid
+
+}
+{  //brute force : O(N*M*K)
+    var replaceElements = function (arr) {
+        if (arr.length < 2) return [-1];
+
+        const getNextMaxValueIndex = (idx, arr) => { //O(M)
+            let p = idx;
+            for (let i = idx; i < arr.length; i++) {
+                if (arr[p] < arr[i]) {
+                    p = i;
+                }
+            }
+            return p;
+        }
+
+        for (let i = 0; i < arr.length; i++) { //O(N)
+
+            let currIdx = i;
+
+            let nextMaxValueIndex = getNextMaxValueIndex(currIdx + 1, arr);
+
+            while (i < nextMaxValueIndex) { //O(K)
+                arr[i] = arr[nextMaxValueIndex];
+                i++; // when finished while loop, i need to back one step
+            }
+            i--;  // i back one step/
+        }
+
+        arr[arr.length - 1] = -1;
+        return arr;
+    };
+    //High Five
+    //Special Positions in a Binary Matrix
+    //Get Biggest Three Rhombus Sums in a Grid
+}
