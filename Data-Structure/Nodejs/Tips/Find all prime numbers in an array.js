7e1b12b0257ee08430794e5c5f4ddc2b0b7e8c93
xing wang <uwspstar@gmail.com>
2021-07-05 06:28:15 -0500

    daily

---
+++ Data-Structure/Nodejs/Tips/Find all prime numbers in an array.js
@@ -0,0 +1,14 @@
+{
+    const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    function isPrime(num) {
+        for (let i = 2; num > i; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return num > 1;
+    }
+
+    console.log(array.filter(isPrime)); // [2, 3, 5, 7]
+}
