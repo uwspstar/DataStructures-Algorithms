4728de79d13914c433e558f3d51c845ac46601eb
xing wang <uwspstar@gmail.com>
2022-01-29 22:48:05 -0600

    daily

---
+++ Data-Structure/All-In-One-LeetCode/00189. Rotate Array/189. Rotate Array.js
@@ -0,0 +1,49 @@
+// 189. Rotate Array
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+/*
+Let n = 7 and k = 3
+
+Original List                   : 1 2 3 4 5 6 7
+After reversing all numbers     : 7 6 5 4 3 2 1
+After reversing first k numbers : 5 6 7 4 3 2 1
+After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+
+*/
+
+const reverse = (arr, start = 0, end = arr.length - 1) => {
+    while (start < end) {
+        const tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
+        start++;
+        end--;
+    }
+}
+
+var rotate = function (nums, k) {
+    k %= nums.length
+    reverse(nums);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k)
+};
