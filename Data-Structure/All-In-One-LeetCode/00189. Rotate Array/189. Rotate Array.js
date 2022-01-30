16174a3c717706bcc1f27b14c22508f8d09afce8
xing wang <uwspstar@gmail.com>
2022-01-29 22:49:01 -0600

    daily

--- Data-Structure/All-In-One-LeetCode/00189. Rotate Array/189. Rotate Array.js
+++ Data-Structure/All-In-One-LeetCode/00189. Rotate Array/189. Rotate Array.js
@@ -41,6 +41,15 @@ const reverse = (arr, start = 0, end = arr.length - 1) => {
     }
 }
 
+/*
+Let n = 7 and k = 3
+
+Original List                   : 1 2 3 4 5 6 7
+After reversing all numbers     : 7 6 5 4 3 2 1
+After reversing first k numbers : 5 6 7 4 3 2 1
+After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+
+*/
 var rotate = function (nums, k) {
     k %= nums.length
     reverse(nums);
