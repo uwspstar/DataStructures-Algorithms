808a33a583a29ca06adcd8c575f9f3fcd28adea0
xing wang <uwspstar@gmail.com>
2022-02-27 21:10:12 -0600

    daily

---
+++ Data-Structure/Alg-Study-Plan/Top FaceBook/01570. Dot Product of Two Sparse Vectors/1570. Dot Product of Two Sparse Vectors.js
@@ -0,0 +1,62 @@
+// 1570. Dot Product of Two Sparse Vectors
+/*
+Given two sparse vectors, compute their dot product.
+
+Implement class SparseVector:
+
+SparseVector(nums) Initializes the object with the vector nums
+dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+Follow up: What if only one of the vectors is sparse?
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+Output: 8
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+Example 2:
+
+Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+Output: 0
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+Example 3:
+
+Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+Output: 6
+*/
+{
+    class SparseVector {
+        constructor(nums) {
+            this.pairs = [];//separate storage for each vector
+            for (let i = 0; i < nums.length; i++) {
+                if (nums[i] !== 0) {
+                    this.pairs.push([i, nums[i]]);
+                }
+            }
+        }
+
+        dotProduct(vec) { //sparse vector is being passed in that has our *pairs* in it 
+            let res = 0, p = 0, q = 0;
+            let M = this.pairs.length, N = vec.pairs.length
+
+            while (p < M && q < N) {
+
+                if (this.pairs[p][0] === vec.pairs[q][0]) {//same index for both storage 
+                    res = res + (this.pairs[p][1] * vec.pairs[q][1]);
+                    p++;
+                    q++;
+                } else if (this.pairs[p][0] > vec.pairs[q][0]) {
+                    q++;  // q needs to catch up p
+                } else {
+                    p++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Data-Structure/Alg-Study-Plan/Top FaceBook/01762. Buildings With an Ocean View/1762. Buildings With an Ocean View.js
@@ -0,0 +1,47 @@
+// 1762. Buildings With an Ocean View
+/*
+There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions. Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+
+ 
+
+Example 1:
+
+Input: heights = [4,2,3,1]
+Output: [0,2,3]
+Explanation: Building 1 (0-indexed) does not have an ocean view because building 2 is taller.
+Example 2:
+
+Input: heights = [4,3,2,1]
+Output: [0,1,2,3]
+Explanation: All the buildings have an ocean view.
+Example 3:
+
+Input: heights = [1,3,2,4]
+Output: [3]
+Explanation: Only building 3 has an ocean view.
+*/
+// monotonous
+{
+    //Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+    var findBuildings = function (arr) {
+        let N = arr.length;
+        let ans = [0]; // same the idx
+
+        for (let i = 1; i < N; i++) {
+            // If the current building is taller, 
+            // it will block the shorter building's ocean view to its left.
+            // So we pop all the shorter buildings that have been added before.
+            let curr = arr[i];
+            while (arr[i] >= arr[ans[ans.length - 1]]) {
+                ans.pop();
+            }
+            ans.push(i);
+        }
+
+        return ans;
+    };
+}
