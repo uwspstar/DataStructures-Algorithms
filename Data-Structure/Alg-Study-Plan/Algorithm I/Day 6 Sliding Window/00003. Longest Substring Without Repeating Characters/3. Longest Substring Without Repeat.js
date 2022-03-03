0d34dc8ee5c6af01f93ab6bd4ac60cdac44964ea
xing wang <uwspstar@gmail.com>
2022-03-02 21:35:27 -0600

    daily

--- Data-Structure/Alg-Study-Plan/Algorithm I/Day 6 Sliding Window/00003. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeat.js
+++ Data-Structure/Alg-Study-Plan/Algorithm I/Day 6 Sliding Window/00003. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeat.js
@@ -41,3 +41,19 @@ for (int i = 0; i < n; i++) {
     }
 }
 */
+
+{
+    var lengthOfLongestSubstring = function (s) {
+        let ans = 0;
+        let window = [];
+        for (let i = 0; i < s.length; i++) {
+            let curr = s[i];
+            while (window.indexOf(curr) >= 0) {
+                window.shift();
+            }
+            window.push(curr);
+            ans = Math.max(ans, window.length);
+        }
+        return ans;
+    }
+}
