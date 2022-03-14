ddd91ca8148de54dc06a3e60fa302a17e0283754
xing wang <uwspstar@gmail.com>
2022-03-13 21:43:47 -0500

    daily

---
+++ Data-Structure/Alg-Study-Plan/Algorithm II/Day 5 Sliding Window/00438. Find All Anagrams in a String/Solution.cpp
@@ -0,0 +1,57 @@
+/*
+基本思路
+PS：这道题在《算法小抄》 的第 85 页。
+
+这题和 567. 字符串的排列 一样，只不过找到一个合法异位词（排列）之后将它的起始索引加入结果列表即可。
+
+滑动窗口算法难度略高，具体的算法原理以及算法模板见详细题解。
+
+详细题解：我写了套框架，把滑动窗口算法变成了默写题
+
+标签：滑动窗口，数组双指针
+
+解法代码
+*/
+class Solution {
+    public:
+    vector<int> findAnagrams(string s, string t) {
+        unordered_map<char, int> need, window;
+        for (char c : t) need[c]++;
+
+        int left = 0, right = 0;
+        int valid = 0;
+        vector<int> res; // 记录结果
+        while (right < s.size()) {
+            char c = s[right];
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.count(c)) {
+                window[c]++;
+                if (window[c] == need[c])
+                    valid++;
+            }
+            // 判断左侧窗口是否要收缩
+            while (right - left >= t.size()) {
+                // 当窗口符合条件时，把起始索引加入 res
+                if (valid == need.size())
+                    res.push_back(left);
+                char d = s[left];
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.count(d)) {
+                    if (window[d] == need[d])
+                        valid--;
+                    window[d]--;
+                }
+            }
+        }
+        return res;
+    }
+};
+/*
+类似题目：
+
+76. 最小覆盖子串（困难）
+567. 字符串的排列（中等）
+3. 无重复字符的最长子串（中等）
+*/
