0fe88c2c870b6ce3f11f1d923472e79f4fe24e43
xing wang <uwspstar@gmail.com>
2022-04-07 19:29:09 -0500

    daily

---
+++ Data-Structure/_All-In-One-LeetCode/00703. Kth Largest Element in a Stream/703. Kth Largest Element in a Stream.js
@@ -0,0 +1,46 @@
+// 703. Kth Largest Element in a Stream
+/*
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+*/
+{
+    var KthLargest = function (k, nums) {
+        this.pq = new MinPriorityQueue(); // V
+        this.k = k;
+        nums.map(x => this.add(x));
+    };
+
+    KthLargest.prototype.add = function (val) {
+        if (this.pq.size() < this.k) {
+            this.pq.enqueue(val);
+        } else if (this.pq.front().element < val) {
+            this.pq.enqueue(val);
+            this.pq.dequeue();
+        }
+        return this.pq.front().element;
+    };
+
+    // Finding MK Average (H)
+    // Sequentially Ordinal Rank Tracker (H)
+}
