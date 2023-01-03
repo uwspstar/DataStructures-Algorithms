a8a658a58ef794d9ec1812075187b130b5bf3bfe
xing wang <uwspstar@gmail.com>
2023-01-03 07:46:41 -0600

    daily

--- Data-Structure/_All-In-One-LeetCode/00217. Contains Duplicate/217. Contains Duplicate.py
+++ Data-Structure/_All-In-One-LeetCode/00217. Contains Duplicate/217. Contains Duplicate.py
@@ -1,8 +1,11 @@
 from typing import List
 
+# s1 t: O(N) s: O(N)
+
 
 class Solution:
     def contains_duplicate(self, nums: List[int]) -> bool:
+        # set: Build an unordered collection of unique elements.
         hash_set = set()
         for n in nums:
             if n in hash_set:
@@ -15,3 +18,14 @@ class Solution:
 sl = Solution()
 print(sl.contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  # true
 print(sl.contains_duplicate([1, 3, 4, 2]))  # false
+
+"""
+(class) set()
+
+set(__iterable: Iterable, /)
+
+set() -> new empty set object set(iterable) -> new set object
+
+Build an unordered collection of unique elements.
+"""
+# s2, sorting : O(NlogN)
