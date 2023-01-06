813ff8255ba03a7a9dfd6bb99ea4758d5ed9d848
xing wang <uwspstar@gmail.com>
2023-01-06 06:35:03 -0600

    daily

--- Data-Structure/_All-In-One-LeetCode/00125. Valid Palindrome/125. Valid Palindrome.py
+++ Data-Structure/_All-In-One-LeetCode/00125. Valid Palindrome/125. Valid Palindrome.py
@@ -10,3 +10,23 @@ class Solution:
             if c.isalnum():
                 new_str += c.lower()  # lower
         return new_str == new_str[::-1]  # reverse
+
+# two pointers
+
+
+def isPalindrome(self, s: str) -> bool:
+    left, right = 0, len(s)-1
+
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+
+        if left < right and s[left].lower() != s[right].lower():
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
